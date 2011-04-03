4adfb2f223af7bc8bf5203348138854cce6e8e28
Mads Hartmann <mads379@gmail.com>
2011-04-03 21:01:18 +0200

    Added a bunch of automated tests of the java parser

--- automated-tests/dk/itu/sdg/javaparser/JavaParser.scala
+++ automated-tests/dk/itu/sdg/javaparser/JavaParser.scala
@@ -7,42 +7,63 @@ import java.io._
 import scala.util.parsing.input.StreamReader
 import dk.itu.sdg.javaparser._
 
-class JavaParserSpec extends FlatSpec with ShouldMatchers with JavaOutputter with JavaAST {
+/*
+  Parse each file in automated-tests/resources/javaparser/source and
+  test that the result is equal to the contents of the file with the same
+  name in the automated-tests/resources/javaparser/expected folder.
+*/
+
+class JavaParserSpec extends FlatSpec with ShouldMatchers 
+                                      with JavaOutputter 
+                                      with JavaAST {
 
   System.setProperty("file.encoding", "UTF-8")
 
-  /*
-    Parse each file in automated-tests/resources/javaparser/source and
-    test that result is equal to the contents of the file with the same
-    name in the automated-tests/resources/javaparser/expected folder.
-  */
-  "Result of parsing all files in source" should "equal to the contents of the associated file in expected" in {
+  "Result of parsing all files in source" should 
+  "equal to the contents of the associated file in expected" in {
 
     val sourcePath = List("automated-tests", "resources", "javaparser", "source").mkString(File.separator)
-    val source = new File(sourcePath)
+    val source     = new File(sourcePath)
 
     source.listFiles().foreach { file =>
-      val in = StreamReader(new InputStreamReader(new FileInputStream(file)))
-      val result = FinishAST.doitHelper(parseH(in)).map(out(_, 0)).reduceLeft(_ + "" + _).trim()
-      val expected = readContentsOfFile(file).trim()
-      result should equal(expected)
+      val in       = StreamReader(new InputStreamReader(new FileInputStream(file)))
+      val result   = FinishAST.doitHelper(parseH(in)).map(out(_, 0)).reduceLeft(_ + " " + _)                                    
+      val expected = getExpectedOutputOfFile(file)
+            
+      simplify(result) should equal(simplify(expected))
     }
   }
 
-  // given a file in sources it will find the file with the expected result of parsing
-  // the file.
+  /*
+    simplifies strings so they're comparable
+  */
+  def simplify(text: String): String = {
+    text.split('\n').filterNot(_ == "")
+                    .reduceLeft(_ + " " + _)
+                    .replace("    "," ")
+                    .replace("   "," ")
+                    .replace("  "," ")
+                    .trim()
+  }
+
+  /*
+    given a file in sources it will find the file with the expected result of 
+    parsing the file.
+  */
   def getExpectedOutputOfFile(file : File) : String = {
     val path = List("automated-tests", "resources", "javaparser", "expected", file.getName).mkString(File.separator)
     readContentsOfFile(new File(path))
   }
 
-  // Returns the contents of a file as a String
+  /*
+    Returns the contents of a file as a String
+  */
   def readContentsOfFile(file : File) : String = {
     val reader = new BufferedReader(new FileReader(file))
     var text = ""
     var line = reader.readLine()
     while (line != null) {
-      text = text + line
+      text = text + " " + line
       line = reader.readLine()
     }
     text
