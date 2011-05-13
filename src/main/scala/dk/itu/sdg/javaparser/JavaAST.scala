ad6e3eced20d368a30865196370c9f89a311a260
Hannes <hannes@mehnert.org>
2011-05-13 11:37:38 +0200

    move JModifier to a separate file use JModifier in SimpleJavaAST as well add map for local variables of constructor and method definitions in SimpleJavaAST

--- src/main/scala/dk/itu/sdg/javaparser/JavaAST.scala
+++ src/main/scala/dk/itu/sdg/javaparser/JavaAST.scala
@@ -4,53 +4,6 @@ package dk.itu.sdg.javaparser
 
 sealed abstract class JStatement { }
 
-sealed abstract class JModifier()
-
-case class Private()      extends JModifier
-case class Protected()    extends JModifier
-case class Public()       extends JModifier
-case class Final()        extends JModifier
-case class Abstract()     extends JModifier
-case class Static()       extends JModifier
-case class Native()       extends JModifier
-case class Transient()    extends JModifier
-case class Volatile()     extends JModifier
-case class Synchronized() extends JModifier
-case class Strict()       extends JModifier
-
-object JModifier extends JavaTerms {
-      
-  def apply(str: String): Option[JModifier] = str match {
-    case "private"      => Some(Private())
-    case "protected"    => Some(Protected())
-    case "public"       => Some(Public())
-    case "final"        => Some(Final())
-    case "abstract"     => Some(Abstract())
-    case "static"       => Some(Static())
-    case "native"       => Some(Native())
-    case "transient"    => Some(Transient())
-    case "volatile"     => Some(Volatile())
-    case "synchronized" => Some(Synchronized())
-    case "strict"       => Some(Strict())
-    case _              => None
-  }
-  
-  def unapply(modifier: JModifier): Option[String] = modifier match {
-    case Private()      => Some("private")
-    case Protected()    => Some("protected")
-    case Public()       => Some("public")
-    case Final()        => Some("final")
-    case Abstract()     => Some("abstract")
-    case Static()       => Some("static")
-    case Native()       => Some("native")
-    case Transient()    => Some("transient")
-    case Volatile()     => Some("volatile")
-    case Synchronized() => Some("synchronized")
-    case Strict()       => Some("strict")
-    case _              => None
-  }
-}
-
 case class JClassDefinition (modifiers : Set[JModifier], id : String, superclass : String, interfaces : List[String], body : List[JStatement], outerclass : Option[String]) extends JStatement
 case class JInterfaceDefinition (modifiers : Set[JModifier], id : String, interfaces : List[String], body : List[JStatement]) extends JStatement
 
--- src/main/scala/dk/itu/sdg/javaparser/SimpleJavaAST.scala
+++ src/main/scala/dk/itu/sdg/javaparser/SimpleJavaAST.scala
@@ -2,15 +2,17 @@
 
 package dk.itu.sdg.javaparser
 
-sealed abstract class SJDefinition (modifiers : List[String]) { }
-case class SJClassDefinition (modifiers : List[String], id : String, superclass : String, interfaces : List[String], body : List[SJBodyDefinition], outerclass : Option[String]) extends SJDefinition (modifiers)
-case class SJInterfaceDefinition (modifiers : List[String], id : String, interfaces : List[String], body : List[SJBodyDefinition]) extends SJDefinition (modifiers)
+sealed abstract class SJDefinition () { }
+case class SJClassDefinition (modifiers : Set[JModifier], id : String, superclass : String, interfaces : List[String], body : List[SJBodyDefinition], outerclass : Option[String]) extends SJDefinition
+case class SJInterfaceDefinition (modifiers : Set[JModifier], id : String, interfaces : List[String], body : List[SJBodyDefinition]) extends SJDefinition
 
-sealed abstract class SJBodyDefinition (modifiers : List[String]) { }
-case class SJFieldDefinition (modifiers : List[String], id : String, jtype : String, init : Option[SJStatement]) extends SJBodyDefinition (modifiers)
-case class SJMethodDefinition (modifiers : List[String], id : String, jtype : String, parameters : List[SJArgument], body : SJStatement) extends SJBodyDefinition (modifiers)
-case class SJConstructorDefinition (modifiers : List[String], jtype : String, parameters : List[SJArgument], body : SJStatement) extends SJBodyDefinition (modifiers)
-case class SJBodyBlock (modifiers : List[String], body : SJStatement) extends SJBodyDefinition (modifiers)
+import scala.collection.mutable.HashMap
+
+sealed abstract class SJBodyDefinition () { }
+case class SJFieldDefinition (modifiers : Set[JModifier], id : String, jtype : String, init : Option[SJStatement]) extends SJBodyDefinition
+case class SJMethodDefinition (modifiers : Set[JModifier], id : String, jtype : String, parameters : List[SJArgument], body : SJStatement, localvariables : HashMap[String, String]) extends SJBodyDefinition
+case class SJConstructorDefinition (modifiers : Set[JModifier], jtype : String, parameters : List[SJArgument], body : SJStatement, localvariables : HashMap[String, String]) extends SJBodyDefinition
+case class SJBodyBlock (modifiers : Set[JModifier], body : SJStatement) extends SJBodyDefinition
 
 sealed case class SJArgument (id : String, jtype : String)
 
@@ -18,13 +20,13 @@ sealed abstract class SJStatement
 case class SJAssert (assertion : SJExpression) extends SJStatement
 case class SJAssignment (left : SJVariableAccess, right : SJStatement) extends SJStatement
 case class SJFieldWrite (variable : SJVariableAccess, field : String, value : SJExpression) extends SJStatement
+case class SJFieldAccess (variable : SJVariableAccess, field : String) extends SJStatement
 case class SJReturn (ret : SJExpression) extends SJStatement
 case class SJBinding (name : String, jtype : String, init : Option[SJStatement]) extends SJStatement
 case class SJWhile (test : SJExpression, body : List[SJStatement]) extends SJStatement
 case class SJConditional (test : SJExpression, consequent : List[SJStatement], alternative : List[SJStatement]) extends SJStatement
 case class SJCall (receiver : SJVariableAccess, fun : String, arguments : List[SJExpression]) extends SJStatement
 case class SJNewExpression (jtype : String, arguments : List[SJExpression]) extends SJStatement
-case class SJFieldAccess (variable : SJVariableAccess, field : String) extends SJStatement
 
 trait SJExpression extends SJStatement
 case class SJBinaryExpression (operation : String, left : SJExpression, right : SJExpression) extends SJExpression
