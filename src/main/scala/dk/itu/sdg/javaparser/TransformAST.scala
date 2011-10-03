8941d23ccb61e4ad01626a3dfcd10f63edc694b7
Mads Hartmann <mads379@gmail.com>
2011-10-03 19:49:14 +0200

    Moving the lvars reset into transform.
    Proposed by Hannes in an email. It does seems to be a more
    fitting place for the resetting.

--- src/main/scala/dk/itu/sdg/javaparser/TransformAST.scala
+++ src/main/scala/dk/itu/sdg/javaparser/TransformAST.scala
@@ -182,6 +182,7 @@ object FinishAST extends JavaTerms
    * Main entry point for transforming the AST.
    */
   def transform (x : Any, outer : Option[String]) : List[JStatement] = {
+    lvars = Set[String]() // Resetting local variables because we're now converting a new class.
     x match {
       case Nil     => Nil
       case x ~ y   => transform(x, outer) ++ transform(y, outer)
@@ -204,7 +205,6 @@ object FinishAST extends JavaTerms
    * respectively. Anything else will throw an exception
    */
   def transformClassOrInterface (x : Any, outer : Option[String], modifiers : Set[JModifier] = Set()) : JStatement = {
-    lvars = Set[String]() // Resetting local variables because we're now converting a new class.
     x match {
       case JClass(id, jtype, superclass, interfaces, bodyp) => {
         log.info("transforming a JClass, ranging (pos) " + x.asInstanceOf[JClass].pos)
