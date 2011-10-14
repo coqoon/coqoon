f636a800082af3423c46757e1bf15d8ec5598703
Mads Hartmann <mads379@gmail.com>
2011-10-14 11:34:54 +0200

    Added a test for the SCC algorithm

--- src/main/scala/dk/itu/sdg/analysis/Graph.scala
+++ src/main/scala/dk/itu/sdg/analysis/Graph.scala
@@ -108,43 +108,4 @@ object Graph {
   
   def adjacent(vertex: Vertex, state: State): List[Vertex] = 
     for { edge <- state.graph.edges if edge.from == vertex } yield edge.to 
-  
-  /* Playground. */
-  def main(args: Array[String]): Unit = {
-
-    // Example taken from page 187, figure 5.13
-
-    val v1 = Vertex("V1")
-    val v2 = Vertex("V2")
-    val v3 = Vertex("V3")
-    val v4 = Vertex("V4")
-    val v5 = Vertex("V5")
-    val v6 = Vertex("V6")
-    val v7 = Vertex("V7")
-    val v8 = Vertex("V8")
-    
-    val vertices = List(v1,v2,v3,v4,v5,v6,v7,v8)
-    
-    val edges = List(
-                  Edge(v1, v2),
-                  Edge(v1, v5),
-                  Edge(v1, v4),
-                  Edge(v2, v3),
-                  Edge(v2, v4),
-                  Edge(v3, v1),
-                  Edge(v4, v3),
-                  Edge(v5, v4),
-                  Edge(v6, v8),
-                  Edge(v6, v7),
-                  Edge(v7, v5),
-                  Edge(v8, v4),
-                  Edge(v8, v6),
-                  Edge(v8, v7))
-    
-    val g = G(v1, vertices, edges)
-
-    
-    println("found components: " + components(g).mkString("\n","\n",""))
-    
-  }
 }
