a80755147aaedffa04b406ebaacf5132fe9f6cce
Hannes <hannes@mehnert.org>
2011-11-06 13:48:36 -0500

    another test case which passes

--- src/test/scala/dk/itu/sdg/javaparser/SimpleJavaASTToCoq.scala
+++ src/test/scala/dk/itu/sdg/javaparser/SimpleJavaASTToCoq.scala
@@ -7,12 +7,19 @@ import org.scalatest.matchers.ShouldMatchers
 import scala.collection.immutable.HashMap
 
 class SimpleJavaASTSpec extends ASTSpec {
-  //coqoutput(<>, false, <>);
-
   "simple class definition" should "produce some coq definition" in {
     val tst = List(SJClassDefinition(Set(), "Foo", "", Nil, List(), None, HashMap()))
     val expected = List("Module Foo <: PROGRAM.",
 "Definition Foo := Build_Class (SS.empty) (SM.empty _).",
+"""Definition Prog := Build_Program (SM.add "Foo" Foo (SM.empty _)).""",
+"End Foo.", "")
+    FinishAST.coqoutput(tst, false, "Foo") should equal(expected)
+  }
+
+  "simple class definition with a field" should "produce some coq definition" in {
+    val tst = List(SJClassDefinition(Set(), "Foo", "", Nil, List(SJFieldDefinition(Set(), "a", "Object")), None, HashMap("a" -> "Object")))
+    val expected = List("Module Foo <: PROGRAM.",
+"""Definition Foo := Build_Class (SS.add "a" (SS.empty)) (SM.empty _).""",
 """Definition Prog := Build_Program (SM.add "Foo" Foo (SM.empty _)).""",
 "End Foo.", "")
     FinishAST.coqoutput(tst, false, "Foo") should equal(expected)
