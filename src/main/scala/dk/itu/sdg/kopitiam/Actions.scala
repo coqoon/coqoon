6cb33a54e32e18b7be3b24b75a465c0b5e6b29e4
Hannes Mehnert <hannes@mehnert.org>
2013-02-18 11:10:59 +0100

    revert bad commit

--- src/main/scala/dk/itu/sdg/kopitiam/Actions.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/Actions.scala
@@ -677,8 +677,7 @@ object CoqOutputDispatcher extends CoqCallback {
           EclipseBoilerPlate.unmark
           JavaPosition.unmark
         }
-        if (! CoqStepNotifier.active)
-          ActionDisabler.enableMaybe
+        ActionDisabler.enableMaybe
         if (!monoton && token.context.length == 0 && goalviewer != null)
           goalviewer.clear
       case CoqGoal(n, goals) =>
