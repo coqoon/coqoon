32d6b419889480075056b9d879e3223b322a4b9c
Alec Faithfull <alef@itu.dk>
2013-05-01 13:30:06 +0200

    Moved the Java proof initialisation handler into JavaEditorHandlers
    No actions remain in Actions.scala, so this commit also renames that file
    to JavaEditorState

--- src/main/scala/dk/itu/sdg/kopitiam/JavaEditorHandlers.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/JavaEditorHandlers.scala
@@ -1,3 +1,5 @@
+/* (c) 2010-2011 Hannes Mehnert */
+
 package dk.itu.sdg.kopitiam
 
 import org.eclipse.ui.texteditor.ITextEditor
@@ -15,6 +17,47 @@ abstract class JavaEditorHandler extends EditorHandler {
 }
 
 import org.eclipse.core.commands.ExecutionEvent
+
+class VerifyMethodHandler extends JavaEditorHandler
+    with EclipseJavaHelper with CoreJavaChecker {
+  import org.eclipse.jface.text.ITextSelection
+  override def execute (ev : ExecutionEvent) : Object = {
+    if (isEnabled()) {
+      //plan:
+      // a: get project
+      val jes = getState
+      val prov = editor.getDocumentProvider
+      val doc = prov.getDocument(editor.getEditorInput)
+      val bla = getRoot(editor.getEditorInput)
+      val cu = getCompilationUnit(bla)
+      jes.setCompilationUnit(Some(cu))
+      jes.method.map(_ => { jes.setUnderway(None); jes.setMethod(None) })
+      // a': CoreJava checking!
+      if (checkAST(jes, cu, doc)) { //no errors!
+        // b: if outdated coqString: translate -- need to verify outdated...
+        // c: find method and statement we want to prove
+        if (walkAST(jes, cu, doc)) { //no errors!
+          val selection = editor.getSelectionProvider.getSelection.asInstanceOf[ITextSelection]
+          val off = selection.getOffset
+          val node = findASTNode(cu, off, 0)
+          val md = findMethod(node)
+          md match {
+            case None => EclipseBoilerPlate.warnUser("Cursor not inside of method", "Please put the cursor inside of the method to verify")
+            case Some(x) =>
+              if (jes.completedMethods.contains(x))
+                EclipseBoilerPlate.warnUser("Already proven", "Sorry, this method was already proven")
+              else {
+                jes.setMethod(Some(x))
+                new JavaProofInitialisationJob(jes).schedule
+                //proj.proveMethod(x)
+              }
+          }
+        }
+      }
+    }
+    null
+  }
+}
 
 class JavaStepForwardHandler
     extends JavaEditorHandler with EclipseJavaHelper with JavaASTUtils {
