15c2c1f4e61e68f296a719a61c337a00fe438b29
Alec Faithfull <alef@itu.dk>
2013-05-02 13:52:49 +0200

    Moved the CoqTopContainer trait to EditorHandlers.scala

--- src/main/scala/dk/itu/sdg/kopitiam/CoqEditorHandlers.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/CoqEditorHandlers.scala
@@ -19,35 +19,7 @@ case class CoqStep(
     coqTop.interp(false, false, text)
   } else CoqTypes.Good("")
 }
-    
-trait CoqTopContainer {
-  def coqTop : CoqTopIdeSlave_v20120710
-  
-  private var goals_ : Option[CoqTypes.goals] = None
-  def goals = goals_
-  def setGoals(g : Option[CoqTypes.goals]) = {
-    goals_ = g
-    fireChange(CoqTopContainer.PROPERTY_GOALS)
-  }
-  
-  import org.eclipse.ui.IPropertyListener
-  private var listeners = Set[IPropertyListener]()
-  def addListener(l : IPropertyListener) = (listeners += l)
-  def removeListener(l : IPropertyListener) = (listeners -= l)
-  def fireChange(propertyID : Int) =
-    listeners.map(_.propertyChanged(this, propertyID))
-  
-  private var busy_ : Boolean = false
-  def busy : Boolean = busy_
-  def setBusy(b : Boolean) : Unit = {
-    busy_ = b
-    fireChange(CoqTopContainer.PROPERTY_BUSY)
-  }
-}
-object CoqTopContainer {
-  final val PROPERTY_BUSY = 979
-  final val PROPERTY_GOALS = 1979
-}
+   
     
 import scala.collection.mutable.Stack
 trait Editor extends CoqTopContainer with org.eclipse.ui.IEditorPart {
