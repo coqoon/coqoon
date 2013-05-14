c62e2f0c38c85646f7aa6ab23b412c0749b4d745
Alec Faithfull <alef@itu.dk>
2013-05-14 15:58:59 +0200

    Invalidate the Java editor's text presentation by using an internal class
    This hack is a slightly more reckless version of what the Coq editor is
    already doing, so finding a better approach would improve both editors

--- src/main/scala/dk/itu/sdg/kopitiam/JavaEditorState.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/JavaEditorState.scala
@@ -75,6 +75,9 @@ class JavaEditorState(val editor : ITextEditor) extends CoqTopContainer {
     try {
       f(model)
     } finally model.disconnect(doc)
+    println(editor.getAdapter(classOf[org.eclipse.jface.text.source.ISourceViewer]))
+    editor.asInstanceOf[org.eclipse.jdt.internal.ui.javaeditor.JavaEditor].
+        getViewer.invalidateTextPresentation /* XXX */
   }
   
   import org.eclipse.jface.text.Position
