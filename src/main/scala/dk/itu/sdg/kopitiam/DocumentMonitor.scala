01d7b288fe267cfce9c60df41d57fe48e897f306
Alec Faithfull <alef@itu.dk>
2013-05-22 11:07:31 +0200

    Trimmed most of the unnecessary code from DocumentMonitor
    DocumentMonitor is no longer a document monitor; now it just listens
    aimlessly for workbench part events. It should probably be deleted soon

--- src/main/scala/dk/itu/sdg/kopitiam/DocumentMonitor.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/DocumentMonitor.scala
@@ -13,42 +13,14 @@
 
 package dk.itu.sdg.kopitiam
 
-import org.eclipse.jface.text.IDocumentListener
 import org.eclipse.ui.{IPartListener2,IWindowListener}
-import org.eclipse.jface.text.IPainter
 
-object DocumentMonitor extends IPartListener2 with IWindowListener with IDocumentListener with EclipseUtils {
-  import org.eclipse.ui.{IWorkbenchPartReference,IWorkbenchPart,IWorkbenchWindow,PlatformUI}
-  import org.eclipse.ui.texteditor.ITextEditor
+object DocumentMonitor extends IPartListener2 with IWindowListener with EclipseUtils {
+  import org.eclipse.ui.{IWorkbenchPartReference,IWorkbenchWindow,PlatformUI}
   import org.eclipse.jface.text.DocumentEvent
 
-  import org.eclipse.jface.text.IDocument
-
-  def handlePartRef (p : IWorkbenchPartReference) : Unit = {
-    val t = p.getPart(false)
-    handlePart(t)
-  }
-
-  def handlePart (t : IWorkbenchPart) : Unit = {
-    if (t.isInstanceOf[ITextEditor]) {
-      val txt = t.asInstanceOf[ITextEditor]
-      val doc = txt.getDocumentProvider.getDocument(txt.getEditorInput)
-      doc.addDocumentListener(this)
-    }
-  }
-
-  def init () : Unit = {
-    val wb = PlatformUI.getWorkbench
-    wb.getWorkbenchWindows.map(x => {
-      x.getPartService.addPartListener(this)
-      x.getPages.toList.map(y =>
-        y.getEditorReferences.toList.map(z =>
-          handlePart(z.getEditor(false))))
-    })
-  }
-
-  override def partOpened (part : IWorkbenchPartReference) : Unit =
-    { handlePartRef(part) }
+  def init () : Unit = PlatformUI.getWorkbench.getWorkbenchWindows.map(
+      _.getPartService.addPartListener(this))
 
   override def windowActivated (window : IWorkbenchWindow) : Unit =
     { window.getPartService.addPartListener(this) }
@@ -59,12 +31,13 @@ object DocumentMonitor extends IPartListener2 with IWindowListener with IDocumen
   override def partDeactivated (part : IWorkbenchPartReference) : Unit = { }
   override def partHidden (part : IWorkbenchPartReference) : Unit = { }
   override def partInputChanged (part : IWorkbenchPartReference) : Unit = { }
+  override def partOpened (part : IWorkbenchPartReference) : Unit = { }
   override def partVisible (part : IWorkbenchPartReference) : Unit = { }
   override def windowClosed (window : IWorkbenchWindow) : Unit = { }
   override def windowDeactivated (window : IWorkbenchWindow) : Unit = { }
   override def windowOpened (window : IWorkbenchWindow) : Unit = { }
 
-  override def documentChanged (event : DocumentEvent) : Unit = {
+  private def documentChanged (event : DocumentEvent) : Unit = {
     val doc = event.getDocument
     //Console.println("doc " + doc + " changed [@" + event.getOffset + "], len: " + event.getLength)
     if (false) {
@@ -127,30 +100,10 @@ object DocumentMonitor extends IPartListener2 with IWindowListener with IDocumen
         if (!foundchange) {
           //change to actual java code!
           proj.ASTdirty = true
-          //JavaPosition.unmarkProofs
-          proj.proofShell match {
-            case None =>
-            case Some(x) =>
-              //JavaPosition.unmark
-              //JavaPosition.retract
-              //CoqRetractAction.doitH
-          }
         }
       }
-      if (proj.isCoqModel(doc)) {
-        Console.println("model updated, setting boolean")
-        //JavaPosition.unmarkProofs
-        proj.proofShell match {
-          case None =>
-          case Some(x) =>
-            //JavaPosition.unmark
-            //JavaPosition.retract
-            //CoqRetractAction.doitH
-        }
-        //DocumentState._content = None
-      }
+      if (proj.isCoqModel(doc))
+        (/* do nothing */)
     }
   }
-
-  override def documentAboutToBeChanged (event : DocumentEvent) : Unit = ()
 }
