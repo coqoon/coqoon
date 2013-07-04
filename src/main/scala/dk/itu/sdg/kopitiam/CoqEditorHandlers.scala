coqoon/coqoon

commit 4739e9ec27a0211e78955654477dfba052f9416a

Removed the hard-coded load paths from InitialiseCoqRunner
InitialiseCoqRunner now uses the load paths associated with an ICoqProject;
those are still hard-coded at the moment, but that'll change soon

src/main/scala/dk/itu/sdg/kopitiam/CoqEditorJobs.scala
@@ -18,19 +18,11 @@ class InitialiseCoqRunner(editor : CoqEditor) extends JobRunner[Unit] {
       import org.eclipse.ui.IFileEditorInput
       import org.eclipse.core.resources.IResource
 
-      val input = editor.getEditorInput
-      val res: Option[IResource] =
-        if (input.isInstanceOf[IFileEditorInput]) {
-          Some(input.asInstanceOf[IFileEditorInput].getFile)
-        } else None
-
-      res match {
-        case Some(r) =>
-          ct.interp(false, false, "Add Rec LoadPath \"" +
-              r.getProject.getFolder("src").getLocation.toOSString + "\".")
-        case None =>
-          Console.println("shouldn't happen - trying to get ProjectDir from " +
-            input + ", which is not an IFileEditorInput")
+      if (editor.isInstanceOf[IFileEditorInput]) {
+        val file = editor.asInstanceOf[IFileEditorInput].getFile
+        val cp = ICoqModel.forProject(file.getProject)
+        for (p <- cp.getLoadPath)
+          p.run(ct)
       }
 
       monitor.worked(1)

src/main/scala/dk/itu/sdg/kopitiam/Project.scala
@@ -126,7 +126,7 @@ private class LoadPathContentProvider extends ITreeContentProvider {
   }
   
   override def getChildren(parent : Any) = parent match {
-    case a : ICoqLoadPath => Array(a._tmp_makecmd)
+    case a : ICoqLoadPath => Array((a.coqdir, a.path))
     case _ => Array()
   }
   

src/main/scala/dk/itu/sdg/kopitiam/CoqModel.scala
@@ -79,6 +79,13 @@ trait ICoqModel extends ICoqElement with IParent {
 }
 object ICoqModel {
   def create(root : IWorkspaceRoot) : ICoqModel = new CoqModelImpl(root)
+  
+  private val instance = new CoqModelImpl(
+      org.eclipse.core.resources.ResourcesPlugin.getWorkspace.getRoot)
+  def getInstance : ICoqModel = instance
+  
+  def forProject(project : IProject) : ICoqProject =
+    getInstance.getProject(project.getName)
 }
 
 private class CoqModelImpl(
@@ -102,14 +109,14 @@ trait ICoqLoadPath {
   def path : String
   def coqdir : Option[String]
   
-  def _tmp_makecmd = "Add Rec LoadPath \"" + path + "\"" + (coqdir match {
-      case Some(dir) => " as " + dir
-      case None => ""
-    }) + "."
-  
-  /* def install(coqtop : CoqTopIdeSlave_v20120703) = {
-    coqtop.interp(true, false, _tmp_makecmd)
-  } */
+  def run(ct : CoqTopIdeSlave_v20120710) : Unit = {
+    val cmd =
+      "Add Rec LoadPath \"" + path + "\"" + (coqdir match {
+        case Some(dir) => " as " + dir
+        case None => ""
+      }) + "."
+    ct.interp(false, false, cmd)
+  }
 }
 
 case class ProjectSourceLoadPath(

commit 021cfd9bfb981fcd3b906ff05c18c673f6e529b6

Perform casting in a safer manner
This commit introduces a dependency on the Scala 2.10 Manifest.unapply
method (and also fixes a mysteriously stupid bug in InitialiseCoqRunner
introduced by commit 4739e9ec27a0211e78955654477dfba052f9416a)

src/main/scala/dk/itu/sdg/kopitiam/CoqEditorHandlers.scala
@@ -25,11 +25,7 @@ import scala.collection.mutable.Stack
 
 abstract class CoqEditorHandler extends EditorHandler {
   override def calculateEnabled = (editor != null && !editor.busy)
-  override def editor : CoqEditor = {
-    if (super.editor.isInstanceOf[CoqEditor]) {
-      super.editor.asInstanceOf[CoqEditor]
-    } else null
-  }
+  override def editor : CoqEditor = TryCast[CoqEditor](super.editor).orNull
 }
 object CoqEditorHandler {
   def getNextCommand(doc : String, offset : Int = 0) : Option[String] = {

src/main/scala/dk/itu/sdg/kopitiam/CoqEditorJobs.scala
@@ -16,14 +16,13 @@ class InitialiseCoqRunner(editor : CoqEditor) extends JobRunner[Unit] {
       monitor.subTask("Adding project loadpath entries")
 
       import org.eclipse.ui.IFileEditorInput
-      import org.eclipse.core.resources.IResource
-
-      if (editor.isInstanceOf[IFileEditorInput]) {
-        val file = editor.asInstanceOf[IFileEditorInput].getFile
+      
+      val input = editor.getEditorInput
+      TryCast[IFileEditorInput](input).map(_.getFile).foreach(file => {
         val cp = ICoqModel.forProject(file.getProject)
         for (p <- cp.getLoadPath)
           ct.interp(false, false, p.asCommand)
-      }
+      })
 
       monitor.worked(1)
     }) match {

src/main/scala/dk/itu/sdg/kopitiam/EclipseStuff.scala
@@ -70,8 +70,7 @@ class CoqCompileRunner(
         new ProcessBuilder(cmdarr : _*).redirectErrorStream(true).start()
       
       var msgs = FunctionIterator.lines(coqcp.getInputStream).mkString("\n")
-      coqcp.waitFor
-      if (coqcp.exitValue != 0)
+      if (coqcp.waitFor != 0)
         fail(new Status(IStatus.ERROR, "dk.itu.sdg.kopitiam", msgs))
       
       monitor.worked(1)

src/main/scala/dk/itu/sdg/kopitiam/EditorHandler.scala
@@ -77,13 +77,8 @@ object CoqTopContainer {
   final val PROPERTY_GOALS = 1979
   
   import org.eclipse.core.runtime.IAdaptable
-  def adapt(a : IAdaptable) : Option[CoqTopContainer] =
-    if (a != null) {
-      val ad = a.getAdapter(classOf[CoqTopContainer])
-      if (ad != null && ad.isInstanceOf[CoqTopContainer]) {
-        Some(ad.asInstanceOf[CoqTopContainer])
-      } else None
-    } else None
+  def adapt(a : IAdaptable) : Option[CoqTopContainer] = Option(a).flatMap(
+      ad => TryCast[CoqTopContainer](ad.getAdapter(classOf[CoqTopContainer])))
 }
 
 trait CoqTopEditorContainer extends CoqTopContainer {

src/main/scala/dk/itu/sdg/kopitiam/JavaEclipse.scala
@@ -6,31 +6,31 @@ object EclipseJavaASTProperties {
   import org.eclipse.jdt.core.dom._
   private val coqDefinition : String = "dk.itu.sdg.kopitiam.coqDefinition"
   def getDefinition(a : ASTNode) : Option[List[String]] = 
-    Option(a.getProperty(coqDefinition)).map { _.asInstanceOf[List[String]] }
+    TryCast[List[String]](a.getProperty(coqDefinition))
   def setDefinition(a : ASTNode, p : Option[List[String]]) =
     a.setProperty(coqDefinition, p.orNull)
     
   private val coqSpecification : String = "dk.itu.sdg.kopitiam.coqSpecification"
   def getSpecification(a : ASTNode) : Option[List[String]] =
-    Option(a.getProperty(coqSpecification)).map { _.asInstanceOf[List[String]] }
+    TryCast[List[String]](a.getProperty(coqSpecification))
   def setSpecification(a : ASTNode, p : Option[List[String]]) =
     a.setProperty(coqSpecification, p.orNull)
   
   private val coqProof : String = "dk.itu.sdg.kopitiam.coqProof"
   def getProof(a : ASTNode) : Option[List[String]] =
-    Option(a.getProperty(coqProof)).map { _.asInstanceOf[List[String]] }
+    TryCast[List[String]](a.getProperty(coqProof))
   def setProof(a : ASTNode, p : Option[List[String]]) =
     a.setProperty(coqProof, p.orNull)
   
   private val coqEnd : String = "dk.itu.sdg.kopitiam.coqEnd"
   def getEnd(a : ASTNode) : Option[String] =
-    Option(a.getProperty(coqEnd)).map { _.asInstanceOf[String] }
+    TryCast[String](a.getProperty(coqEnd))
   def setEnd(a : ASTNode, p : Option[String]) =
     a.setProperty(coqEnd, p.orNull)
   
   private val method : String = "dk.itu.sdg.kopitiam.method"
   def getMethod(a : ASTNode) : Option[MethodDeclaration] =
-    Option(a.getProperty(method)).map { _.asInstanceOf[MethodDeclaration] }
+    TryCast[MethodDeclaration](a.getProperty(method))
   def setMethod(a : ASTNode, p : Option[MethodDeclaration]) =
     a.setProperty(method, p.orNull)
   

src/main/scala/dk/itu/sdg/kopitiam/JavaEditorHandlers.scala
@@ -12,11 +12,7 @@ case class JavaStep(
     override val text : String) extends CoqCommand(text)
 
 abstract class JavaEditorHandler extends EditorHandler {
-  override def editor : ITextEditor = {
-    if (super.editor.isInstanceOf[ITextEditor]) {
-      super.editor.asInstanceOf[ITextEditor]
-    } else null
-  }
+  override def editor : ITextEditor = TryCast[ITextEditor](super.editor).orNull
   
   override def calculateEnabled : Boolean = {
     if (editor == null)

src/main/scala/dk/itu/sdg/kopitiam/JavaEditorJobs.scala
@@ -21,12 +21,7 @@ class JavaProofInitialisationRunner(
       import org.eclipse.core.resources.IResource
 
       val input = jes.editor.getEditorInput
-      val res: Option[IResource] =
-        if (input.isInstanceOf[IFileEditorInput]) {
-          Some(input.asInstanceOf[IFileEditorInput].getFile)
-        } else None
-
-      res match {
+      TryCast[IFileEditorInput](input).map(_.getFile) match {
         case Some(r) =>
           ct.interp(false, false,
             "Add Rec LoadPath \"" +

src/main/scala/dk/itu/sdg/kopitiam/Project.scala
@@ -286,15 +286,11 @@ class CoqBuilder extends IncrementalProjectBuilder {
       filter : IResource => Option[A], f : A => Unit) : Unit = {
     for (i <- folder.members) {
       filter(i).map(f)
-      i match {
-        case i : IContainer => traverse(i, filter, f)
-        case _ =>
-      }
+      TryCast[IContainer](i).foreach(traverse(_, filter, f))
     }
   }
   
-  private def fileFilter(r : IResource) : Option[IFile] = 
-    if (r.isInstanceOf[IFile]) Some(r.asInstanceOf[IFile]) else None
+  private def fileFilter(r : IResource) : Option[IFile] = TryCast[IFile](r)
   
   private def extensionFilter[A <: IResource](ext : String)(r : A) : Option[A] =
     Option(r).filter(_.getFileExtension == ext)
@@ -366,19 +362,16 @@ class FolderCreationRunner(a : IResource) extends JobRunner[Unit] {
   private def create(a : IFolder) : Unit = {
     if (a.exists)
       return
-    a.getParent match {
-      case f : IFolder => create(f)
-      case _ =>
-    }
+    TryCast[IFolder](a.getParent).foreach(create)
     a.create(IResource.NONE, true, null)
   }
   
-  override def doOperation(monitor : SubMonitor) : Unit = {
-    a.getParent match {
-      case f : IFolder => create(f)
-      case _ =>
-    }
-  }
+  override def doOperation(monitor : SubMonitor) : Unit =
+    TryCast[IFolder](a.getParent).foreach(create)
+}
+
+object TryCast {
+  def apply[A](a : Any)(implicit a0 : Manifest[A]) = a0.unapply(a)
 }
 
 import org.eclipse.ui.INewWizard
@@ -495,7 +488,7 @@ import org.eclipse.jface.viewers.TreeViewer
 class LoadPathConfigurationPage
     extends PreferencePage with IWorkbenchPropertyPage {
   private var element : IAdaptable = null
-  override def getElement : IProject = element.asInstanceOf[IProject]
+  override def getElement : IProject = TryCast[IProject](element).get
   override def setElement(element : IAdaptable) = (this.element = element)
   override def createContents(c : Composite) = {
     val tf = new TabFolder(c, SWT.NONE)