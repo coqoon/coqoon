4529161b446bccb00f64bd2310139d2c984087d4
Alec Faithfull <alef@itu.dk>
2013-09-04 16:10:15 +0200

    Added support for sash-based goal presenters
    This commit also converts the two existing goal presenters to use a sash

--- src/main/scala/dk/itu/sdg/kopitiam/GoalViewer.scala
+++ src/main/scala/dk/itu/sdg/kopitiam/GoalViewer.scala
@@ -13,7 +13,7 @@ import org.eclipse.swt.layout.FillLayout
 
 abstract class TabbedGoalPresenter extends GoalPresenter {
   import org.eclipse.swt.widgets.TabFolder
-  
+
   private var goals_ : TabFolder = null
   protected def goals : TabFolder = goals_
   protected def subgoals = goals.getItems()
@@ -21,72 +21,140 @@ abstract class TabbedGoalPresenter extends GoalPresenter {
   override def init(parent : Composite) = {
     goals_ = new TabFolder(parent, SWT.NONE)
   }
-  
-  protected def makeTab() : Unit
-  protected def updateTab(goal : CoqTypes.goal, control : TabItem) : Unit
-  
+
+  protected def makeTab(parent : Composite) : Control
+  protected def updateTab(goal : CoqTypes.goal, control : Control) : Unit
+
   override def render(coqGoals : Option[CoqTypes.goals]) = {
     val goalData = coqGoals match {
       case None => List.empty
       case Some(x) => x.fg_goals
     }
     if (subgoals.length < goalData.length) {
-      while (subgoals.length != goalData.length)
-        makeTab
+      while (subgoals.length != goalData.length) {
+        val ti = new TabItem(goals, SWT.NONE)
+        ti.setControl(makeTab(goals))
+        ti.setText(subgoals.length.toString)
+      }
     } else subgoals.drop(goalData.length).map(_.dispose)
-    goals.pack
-    goalData.zip(subgoals).foreach(a => updateTab(a._1, a._2))
+    goals.layout
+    goalData.zip(subgoals).foreach(a => updateTab(a._1, a._2.getControl))
   }
 
   override def dispose = goals_.dispose
 }
 
-class DefaultGoalPresenter extends TabbedGoalPresenter {
-  override protected def makeTab = {
-    val ti = new TabItem(goals, SWT.NONE)
-    val area = new Composite(goals, SWT.NONE)
-    area.setLayout(new FillLayout(SWT.VERTICAL))
-    ti.setControl(area)
+abstract class SashGoalPresenter extends TabbedGoalPresenter {
+  import org.eclipse.swt.layout.{FormData, FormLayout, FormAttachment}
 
-    new Text(area,
-      SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
-    new Text(area,
-      SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
-    ti.setText(subgoals.length.toString)
+  /* The Composite passed to these methods has a FormLayout */
+  protected def makeTabTop(parent : Composite) : Unit
+  protected def makeTabBottom(parent : Composite) : Unit
+
+  final val sharedSashData = new FormData
+
+  override def init(parent : Composite) = {
+    super.init(parent)
+
+    sharedSashData.left = new FormAttachment(0, 0)
+    sharedSashData.right = new FormAttachment(100, 0)
+    sharedSashData.top = new FormAttachment(50, 0)
   }
-  
-  override protected def updateTab(goal : CoqTypes.goal, control : TabItem) = {
-    val comp = control.getControl().asInstanceOf[Composite]
-    comp.getChildren()(0).asInstanceOf[Text].setText(
+
+  override protected def makeTab(parent : Composite) = {
+    import org.eclipse.swt.widgets.{Sash, Event, Listener}
+
+    val area = new Composite(parent, SWT.NONE)
+    area.setLayout(new FormLayout)
+
+    val top = new Composite(area, SWT.NONE)
+    val sash = new Sash(area, SWT.HORIZONTAL)
+    val bottom = new Composite(area, SWT.NONE)
+
+    top.setLayout(new FillLayout(SWT.VERTICAL))
+    val topData = new FormData
+    topData.left = new FormAttachment(0)
+    topData.right = new FormAttachment(100)
+    topData.top = new FormAttachment(0)
+    topData.bottom = new FormAttachment(sash)
+    top.setLayoutData(topData)
+    makeTabTop(top)
+
+    sash.addListener(SWT.Selection, new Listener {
+      private final val LIMIT = 20 /* px */
+
+      override def handleEvent(ev : Event) {
+        val sashBounds = sash.getBounds
+        val parentBounds = sash.getParent.getClientArea
+        val top = parentBounds.height - sashBounds.height - LIMIT
+        val ny = Math.max(Math.min(ev.y, top), LIMIT)
+        if (ny != sashBounds.y) {
+          sharedSashData.top = new FormAttachment(ny, parentBounds.height, 0)
+          parent.layout(true, true) /* Redraw the other tabs, too */
+        }
+      }
+    })
+    sash.setLayoutData(sharedSashData)
+
+    bottom.setLayout(new FillLayout(SWT.VERTICAL))
+    val bottomData = new FormData
+    bottomData.left = new FormAttachment(0)
+    bottomData.right = new FormAttachment(100)
+    bottomData.top = new FormAttachment(sash)
+    bottomData.bottom = new FormAttachment(100)
+    bottom.setLayoutData(bottomData)
+    makeTabBottom(bottom)
+
+    area
+  }
+
+  protected def updateTab(
+      goal : CoqTypes.goal, top : Composite, bottom : Composite) : Unit
+  override protected final def updateTab(
+      goal : CoqTypes.goal, control : Control) =
+    TryCast[Composite](control).map(_.getChildren()).getOrElse(
+        Array.empty).flatMap(TryCast[Composite]) match {
+      case Array(top, bottom) => updateTab(goal, top, bottom)
+      case _ =>
+    }
+}
+
+class DefaultGoalPresenter extends SashGoalPresenter {
+  override protected def makeTabTop(parent : Composite) =
+    new Text(parent,
+        SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
+
+  override protected def makeTabBottom(parent : Composite) =
+    new Text(parent,
+        SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
+
+  override protected def updateTab(
+      goal : CoqTypes.goal, top : Composite, bottom : Composite) = {
+    top.getChildren()(0).asInstanceOf[Text].setText(
         goal.goal_hyp.mkString("\n"))
-    comp.getChildren()(1).asInstanceOf[Text].setText(goal.goal_ccl)
+    bottom.getChildren()(0).asInstanceOf[Text].setText(goal.goal_ccl)
   }
 }
 
-class TabularGoalPresenter extends TabbedGoalPresenter {
+class TabularGoalPresenter extends SashGoalPresenter {
   import org.eclipse.swt.widgets.{Table, TableItem, TableColumn}
 
-  override protected def makeTab = {
-    val ti = new TabItem(goals, SWT.NONE)
-    val area = new Composite(goals, SWT.NONE)
-    area.setLayout(new FillLayout(SWT.VERTICAL))
-    ti.setControl(area)
-
-    val ta = new Table(area, SWT.BORDER | SWT.MULTI)
+  override protected def makeTabTop(parent : Composite) = {
+    val ta = new Table(parent, SWT.BORDER | SWT.MULTI)
     new TableColumn(ta, SWT.LEFT).setText("Name")
     new TableColumn(ta, SWT.LEFT).setText("Value")
     ta.setColumnOrder(Array(0, 1))
     ta.setLinesVisible(true)
     ta.setHeaderVisible(true)
-    new Text(area,
-      SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
-    ti.setText(subgoals.length.toString)
   }
+  
+  override protected def makeTabBottom(parent : Composite) =
+    new Text(parent,
+      SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL)
 
-  override protected def updateTab(goal : CoqTypes.goal, control : TabItem) = {
-    val comp = control.getControl().asInstanceOf[Composite]
-
-    val table = comp.getChildren()(0).asInstanceOf[Table]
+  override protected def updateTab(
+      goal : CoqTypes.goal, top : Composite, bottom : Composite) = {
+    val table = top.getChildren()(0).asInstanceOf[Table]
     if (table.getItems.length < goal.goal_hyp.length) {
       while (table.getItems.length < goal.goal_hyp.length)
         new TableItem(table, SWT.NONE)
@@ -97,7 +165,7 @@ class TabularGoalPresenter extends TabbedGoalPresenter {
         entry.setText(hypothesis.split(":", 2).map(_.trim))
     })
 
-    comp.getChildren()(1).asInstanceOf[Text].setText(goal.goal_ccl)
+    bottom.getChildren()(0).asInstanceOf[Text].setText(goal.goal_ccl)
   }
 }
 
@@ -106,7 +174,7 @@ import org.eclipse.ui.{IPropertyListener, IPartListener2}
 
 class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
   import org.eclipse.swt.layout.{FormData,FormLayout,FormAttachment}
-  
+
   override def propertyChanged (source : Object, propID : Int) = {
     if (source.isInstanceOf[CoqTopContainer] &&
         propID == CoqTopContainer.PROPERTY_GOALS)
@@ -118,13 +186,13 @@ class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
     super.init(site)
     site.getWorkbenchWindow().getPartService().addPartListener(this)
   }
-  
+
   override def dispose = {
     setPresenter(null)
     getSite.getWorkbenchWindow().getPartService().removePartListener(this)
     super.dispose
   }
-  
+
   import org.eclipse.ui.{IEditorPart, IWorkbenchPart}
   private var activeContainer : Option[CoqTopContainer] = None
   private def setActiveContainer(e : IWorkbenchPart) = {
@@ -141,7 +209,7 @@ class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
         writeGoal(None)
     }
   }
-  
+
   import org.eclipse.ui.IWorkbenchPartReference
   override def partOpened (ref : IWorkbenchPartReference) = {
     val p = ref.getPart(false)
@@ -149,13 +217,13 @@ class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
       setActiveContainer(
           getSite.getWorkbenchWindow().getActivePage().getActiveEditor())
   }
-  
+
   override def partClosed (ref : IWorkbenchPartReference) = {
     val p = ref.getPart(false)
     if (this == p || TryAdapt[CoqTopContainer](p) == activeContainer)
       setActiveContainer(null)
   }
-  
+
   override def partActivated (ref : IWorkbenchPartReference) = {
     val p = ref.getPart(false)
     if (p.isInstanceOf[IEditorPart] &&
@@ -171,16 +239,16 @@ class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
   override def partVisible (part : IWorkbenchPartReference) : Unit = { }
 
   private var presenter : GoalPresenter = null
-  
+
   var comp : Composite = null
 
   import org.eclipse.swt.layout.FillLayout
-  
+
   override def createPartControl (parent : Composite) : Unit = {
     comp = new Composite(parent, SWT.NONE)
     comp.setLayout(new FillLayout())
     setPresenter(new DefaultGoalPresenter)
-    
+
     import org.eclipse.jface.action.{Action, IAction}
     val mm = getViewSite.getActionBars.getMenuManager
     mm.add(new Action("No presenter") {
@@ -209,7 +277,7 @@ class GoalViewer extends ViewPart with IPropertyListener with IPartListener2 {
       writeGoal(activeContainer.flatMap(_.goals))
     }
   }
-  
+
   private def writeGoal (coqGoals : Option[CoqTypes.goals]) : Unit = {
     if (!comp.isDisposed && presenter != null)
       presenter.render(coqGoals)
