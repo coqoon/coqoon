4d7b1dc75f431f1c40c10041669fc88172589c90
Alec Faithfull <alef@itu.dk>
2013-11-12 11:18:23 +0100

    Added a trivial plugin build script for Coqoon
    This is a bodge to get around Scala IDE's unreliable export of plugins, and
    is hilariously unportable -- it assumes that Eclipse has already built the
    .class files, and that you have the Debian developer toolchain installed

---
+++ plugins/make-jars.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Copyright © 2013 Alexander Faithfull.
+#
+# This build script is free software; its author gives unlimited permission
+# to copy and/or distribute it, with or without modifications, as long as this
+# notice is preserved.
+
+getmprop() {
+	grep-dctrl --show-field="$1" --no-field-names "" META-INF/MANIFEST.MF
+}
+
+buildjar() {
+	(
+		cd "$1"
+		shift 1
+
+		name="`getmprop Bundle-Name`"
+		id="`getmprop Bundle-SymbolicName`"; id=${id%%;*}
+		version="`getmprop Bundle-Version`"
+		echo "Building $version of $name ($id)"
+
+		jar cfm "../${id}_$version.jar" META-INF/MANIFEST.MF "$@"
+	)
+}
+
+buildjar dk.itu.coqoon.core \
+	src/ plugin.xml schema/ license.txt -C bin/ dk/
+buildjar dk.itu.coqoon.ui \
+	src/ plugin.xml icons/ -C bin/ dk/
+buildjar dk.itu.sdg.kopitiam \
+	src/ plugin.xml license.txt -C bin/ dk/
