b8b3528cacafd8f7eea0ecb61e4e7f7d6242c11c
Alec Faithfull <alef@itu.dk>
2013-11-15 13:43:53 +0100

    Added a view for exploring the Coq model
    This isn't intended to last very long, but it's a tremendously helpful
    debugging tool

---
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/ModelExplorer.scala
@@ -0,0 +1,57 @@
+/* ModelExplorer.scala
+ * Copyright © 2013 Alexander Faithfull
+ *
+ * You may use, copy, modify and/or redistribute this code subject to the terms
+ * of either the license of Kopitiam or the Apache License, version 2.0 */
+
+package dk.itu.coqoon.ui
+
+import dk.itu.coqoon.core.model._
+import dk.itu.coqoon.core.utilities.TryCast
+
+import org.eclipse.ui.part.ViewPart
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.jface.viewers._
+
+class ModelExplorer extends ViewPart {
+  private var tree : TreeViewer = null
+
+  override def createPartControl(parent : Composite) = {
+    tree = new TreeViewer(parent)
+    tree.setLabelProvider(new LabelProvider)
+    tree.setContentProvider(new ModelContentProvider)
+    tree.setInput(ICoqModel.getInstance)
+  }
+
+  override def setFocus() = Option(tree).foreach(_.getControl.setFocus)
+}
+
+class ModelContentProvider
+    extends ITreeContentProvider with CoqElementChangeListener {
+  private var viewer : Viewer = null
+  private var input : ICoqModel = null
+
+  override def dispose = Option(input).foreach(_.removeListener(this))
+  override def inputChanged(v : Viewer, o : Any, n : Any) = {
+    viewer = v
+    Option(input).foreach(_.removeListener(this))
+    n match {
+      case m : ICoqModel =>
+        input = m
+        m.addListener(this)
+      case _ =>
+        input = null
+    }
+  }
+
+  override def coqElementChanged(ev : CoqElementChangeEvent) =
+    Option(viewer).foreach(_.refresh)
+
+  private def toCE(o : Any) = Option(o).flatMap(TryCast[ICoqElement])
+  override def getChildren(o : Any) = toCE(o).flatMap(
+      TryCast[IParent]).toSeq.flatMap(_.getChildren).toArray
+  override def getElements(o : Any) = getChildren(o)
+  override def getParent(o : Any) = toCE(o).flatMap(_.getParent).orNull
+  override def hasChildren(o : Any) = toCE(o).flatMap(
+      TryCast[IParent]).map(_.hasChildren).getOrElse(false)
+}
