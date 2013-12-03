6af14b127257d73670277b6048812fdf15298377
Alec Faithfull <alef@itu.dk>
2013-12-03 01:26:28 +0100

    Added a new command for jumping to the declaration of an identifier
    This is very much a prototype of this functionality -- there's no index, so
    there's no support for external identifiers

---
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/Navigation.scala
@@ -0,0 +1,61 @@
+package dk.itu.coqoon.ui
+
+import org.eclipse.ui.part.FileEditorInput
+import org.eclipse.core.commands.ExecutionEvent
+import dk.itu.coqoon.ui.utilities.UIUtils
+
+import dk.itu.coqoon.core.model.{ICoqModel, ICoqVernacFile}
+import dk.itu.coqoon.core.utilities.{TryCast, TryAdapt}
+import dk.itu.coqoon.core.coqtop.CoqSentence
+
+class OpenDeclarationHandler extends EditorHandler {
+  override def execute(ev : ExecutionEvent) : AnyRef = {
+    val editor = TryAdapt[CoqEditor](UIUtils.getWorkbench.
+        getActiveWorkbenchWindow.getActivePage.getActiveEditor)
+    editor match {
+      case Some(editor) =>
+        editor.file.flatMap(ICoqModel.getInstance.toCoqElement) match {
+          case Some(f : ICoqVernacFile) =>
+            var (start, end) = (editor.cursorPosition, editor.cursorPosition)
+            while (editor.document.getChar(start - 1).isLetterOrDigit)
+              start -= 1
+            while (editor.document.getChar(end).isLetterOrDigit)
+              end += 1
+            if (start != end) {
+              val identifier = editor.document.get(start, end - start)
+              import dk.itu.coqoon.core.model._
+              def findIdentifier(f : ICoqScriptElement) :
+                  Option[ICoqScriptGroup] = f match {
+                case e : ICoqScriptSentence => None
+                case e : ICoqScriptGroup => e.getDisposition match {
+                  case CoqProofGroup(id) if id == identifier =>
+                    Some(e)
+                  case _ =>
+                    for (i <- e.getChildren;
+                         j <- findIdentifier(i))
+                      return Some(j)
+                    None
+                }
+              }
+              import dk.itu.coqoon.core.utilities.{TryCast, Substring}
+              for (i <- f.getChildren;
+                   j <- findIdentifier(i);
+                   k <- j.getChildren.headOption;
+                   l <- TryCast[ICoqScriptSentence](k)) {
+                val t = l.getText
+
+                val padding = t.takeWhile(_.isWhitespace).length
+                val (start, length) = (t.start + padding, t.length - padding)
+                editor.getViewer.revealRange(start, length)
+                editor.getViewer.setSelectedRange(start, length)
+
+                return null
+              }
+            }
+          case _ =>
+        }
+      case _ =>
+    }
+    null
+  }
+}
