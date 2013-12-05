coqoon/coqoon

commit 22ada8706e772e2c2dda0fb86573b9253dca90f5

Removed the parameter from the ParserStack.popContext method
Clients were essentially obliged to call this method with the return value
of the getInnermostContext method. This commit simplifies things enormously
by always returning the innermost context

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqModelImpl.scala
@@ -426,7 +426,7 @@ private case class CoqVernacFileImpl(
           case (h @ (IdentifiedEndSentence(id), _)) :: tail
               if stack.getInnermostContext == Some(CoqSectionGroup(id)) =>
             pushSentence(h)
-            val (tag, body) = stack.popContext(CoqSectionGroup(id))
+            val (tag, body) = stack.popContext
             stack.push(CoqScriptGroupImpl(
                 tag, body.reverse, CoqVernacFileImpl.this))
             tail
@@ -438,7 +438,7 @@ private case class CoqVernacFileImpl(
           case (h @ (IdentifiedEndSentence(id), _)) :: tail
               if stack.getInnermostContext == Some(CoqModuleGroup(id)) =>
             pushSentence(h)
-            val (tag, body) = stack.popContext(CoqModuleGroup(id))
+            val (tag, body) = stack.popContext
             stack.push(CoqScriptGroupImpl(
                 tag, body.reverse, CoqVernacFileImpl.this))
             tail
@@ -470,15 +470,12 @@ private case class CoqVernacFileImpl(
              * used?) */
             pushSentence(h, i)
             tail
-          case (h @ (ProofEndSentence(_), _)) :: tail =>
-            pushSentence(h)
-            stack.getInnermostContext match {
-              case Some(q @ CoqProofGroup(identifier)) =>
-                val (tag, body) = stack.popContext(q)
-                stack.push(CoqScriptGroupImpl(
-                    tag, body.reverse, CoqVernacFileImpl.this))
-              case _ =>
-            }
+          case (h @ (ProofEndSentence(_), _)) :: tail
+              if stack.getInnermostContext.exists(
+                  _.isInstanceOf[CoqProofGroup]) =>
+            val (tag, body) = stack.popContext
+            stack.push(CoqScriptGroupImpl(
+                tag, body.reverse, CoqVernacFileImpl.this))
             tail
 
           case h :: (i @ (ProofStartSentence(), _)) :: tail =>

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/coqtop/CoqSentence.scala
@@ -193,22 +193,16 @@ class ParserStack[A, B] {
   def push(value : A) = stack +:= Left(value)
   def pushContext(value : B) = stack +:= Right(value)
 
-  def popContext(value : B) : (B, Seq[A]) = popContext(a => value == a)
-  def popContext(f : B => Boolean) : (B, Seq[A]) = {
+  def popContext() : (B, Seq[A]) = {
     var (prefix, remainder) = stack.span {
-      case Right(v) if f(v) => false
-      case Right(v) =>
-        throw new Exception(
-            "Innermost context " + v + " not accepted by comparator")
+      case Right(v) => false
       case _ => true
     }
-    if (remainder.headOption.flatMap(_.right.toOption).map(f) != Some(true))
-      throw new Exception(
-          "" + remainder.headOption + " not acceptable to comparator")
+    if (remainder == Nil)
+      throw new Exception("No active context")
     val tag = remainder.head.right.get
     stack = remainder.tail
-    /* prefix can only contain Lefts at this point */
-    (tag, prefix.map(_.left.get))
+    (tag, prefix.flatMap(_.left.toOption))
   }
 
   def getInnermostContext() : Option[B] =

commit 4cb1bc0eda068a6071804ea399ca4e90c90c764b

Don't strip proof-closing sentences from proof groups
This bug was introduced in commit 22ada8706e772e2c2dda0fb86573b9253dca90f5

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqModelImpl.scala
@@ -479,6 +479,7 @@ private case class CoqVernacFileImpl(
           case (h @ (ProofEndSentence(_), _)) :: tail
               if stack.getInnermostContext.exists(
                   _.isInstanceOf[CoqProofGroup]) =>
+            pushSentence(h)
             val (tag, body) = stack.popContext
             stack.push(CoqScriptGroupImpl(
                 tag, body.reverse, CoqVernacFileImpl.this))