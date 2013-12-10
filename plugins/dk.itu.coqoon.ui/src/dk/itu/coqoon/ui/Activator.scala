94d7d202bee61b0b33a9651a17b1d5dd19d9631b
Alec Faithfull <alef@itu.dk>
2013-12-10 16:07:54 +0100

    Install the package icons into the Coqoon UI plugin's image registry

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/Activator.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/Activator.scala
@@ -25,6 +25,18 @@ class Activator extends AbstractUIPlugin {
     PreferenceConverter.setDefault(
         store, "coqKeywordFg", new RGB(127, 6, 101))
   }
+
+  import ManifestIdentifiers.Images._
+  import dk.itu.coqoon.ui.utilities.UIUtils
+  import org.eclipse.swt.graphics.{Image, ImageData}
+  import org.eclipse.core.runtime.{Path, FileLocator}
+  import org.eclipse.jface.resource.ImageRegistry
+  override protected def initializeImageRegistry(r : ImageRegistry) =
+    for (i <- Seq(
+        PACKAGE_FRAGMENT -> "icons/jdt/package_obj.gif",
+        PACKAGE_FRAGMENT_ROOT -> "icons/jdt/packagefolder_obj.gif"))
+      r.put(i._1, new Image(UIUtils.getDisplay, new ImageData(
+          FileLocator.find(getBundle, new Path(i._2), null).openStream)))
 }
 object Activator {
   private var instance : Activator = _
@@ -42,4 +54,11 @@ object ManifestIdentifiers {
     "dk.itu.coqoon.ui.commands.toggle_coq_flag"
   final val COMMAND_PARAMETER_TOGGLE_COQ_FLAG_NAME =
     "dk.itu.coqoon.ui.commands.toggle_coq_flag.name"
+
+  object Images {
+    final val PACKAGE_FRAGMENT =
+      "dk.itu.coqoon.ui.images.PackageFragment"
+    final val PACKAGE_FRAGMENT_ROOT =
+      "dk.itu.coqoon.ui.images.PackageFragmentRoot"
+  }
 }
