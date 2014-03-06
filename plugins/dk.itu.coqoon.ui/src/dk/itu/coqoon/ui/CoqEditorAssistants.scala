527d0c00f7547e1e23cd73c2cd8213b1afe1a9a9
Alec Faithfull <alef@itu.dk>
2014-03-06 17:05:12 +0100

    Added a new kind of Eclipse text-scanning rule
    The new FuturisticWordRule class and its associated IFuturisticWordDetector
    trait add support for words which have special requirements for trailing
    characters

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/CoqEditorAssistants.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/CoqEditorAssistants.scala
@@ -49,3 +49,50 @@ object ConfigureCoqPathResolution extends IMarkerResolution {
 class ResolutionGenerator extends IMarkerResolutionGenerator {
   override def getResolutions(m : IMarker) = Array(ConfigureCoqPathResolution)
 }
+
+import org.eclipse.jface.text.rules.{
+  IRule, Token, IToken, IWordDetector, ICharacterScanner}
+
+trait IFuturisticWordDetector extends IWordDetector {
+  def isWordEnd(character : Char) : Boolean
+}
+
+class FuturisticWordRule(detector : IFuturisticWordDetector,
+    default : IToken = Token.UNDEFINED) extends IRule {
+  private var tokens : Map[String, IToken] = Map()
+
+  def addWord(word : String, token : IToken) = (tokens += word -> token)
+
+  override def evaluate(scanner : ICharacterScanner) : IToken = {
+    var content : Seq[Char] = Seq()
+    var i_ = scanner.read()
+
+    while (i_ != -1) {
+      val i = i_.asInstanceOf[Char]
+      if (content.isEmpty) {
+        if (detector.isWordStart(i)) {
+          content :+= i
+          i_ = scanner.read()
+        } else {
+          scanner.unread()
+          return Token.UNDEFINED
+        }
+      } else if (detector.isWordPart(i)) {
+        content :+= i
+        i_ = scanner.read()
+      } else {
+        scanner.unread()
+        i_ = -1
+      }
+    }
+
+    while (!content.isEmpty && !detector.isWordEnd(content.last)) {
+      scanner.unread()
+      content = content.dropRight(1)
+    }
+
+    if (!content.isEmpty) {
+      tokens.get(content.mkString).getOrElse(default)
+    } else Token.UNDEFINED
+  }
+}
