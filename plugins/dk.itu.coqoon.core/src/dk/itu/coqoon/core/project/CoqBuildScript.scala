5e7c4f90002f9bb751ff75dca6bf5dab4da8cf5a
Alec Faithfull <alef@itu.dk>
2015-02-10 13:31:36 +0100

    Moved the Python build script management code into a new file
    This commit also upgrades the visibility of the CoqBuildScript object from
    private to public, as a new preferences page to control build script
    generation will soon be added and will depend on that object

---
+++ plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/project/CoqBuildScript.scala
@@ -0,0 +1,133 @@
+/* CoqBuildScript.scala
+ * Utilities for manipulating and updating project build scripts
+ * Copyright © 2015 Alexander Faithfull
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License. */
+
+package dk.itu.coqoon.core.project
+
+import dk.itu.coqoon.core.ManifestIdentifiers
+import dk.itu.coqoon.core.model.{ICoqProject, AbstractLoadPath}
+import org.eclipse.core.runtime.Path
+import org.eclipse.core.resources.IProject
+
+object CoqBuildScript {
+  import org.eclipse.core.runtime.{CoreException, QualifiedName}
+  object WriteBuildScript {
+    final private val ID = new QualifiedName(
+        ManifestIdentifiers.PLUGIN, "writeBuildScript")
+
+    def get(project : IProject) : Option[Boolean] =
+      try {
+        Option(project.getPersistentProperty(ID)).map(
+            java.lang.Boolean.getBoolean)
+      } catch {
+        case c : CoreException =>
+          None
+      }
+
+    def set(project : IProject, b : Option[Boolean]) =
+      try {
+        project.setPersistentProperty(ID, b.map(_.toString).orNull)
+      } catch {
+        case c : CoreException =>
+          /* do nothing */
+      }
+  }
+
+  final val currentVersion = 5
+  final val Version = """^_configure_coqoon_version = (\d+)$""".r
+  def perhapsInstall(project : IProject) : Boolean = {
+    import dk.itu.coqoon.core.Activator
+    import org.eclipse.core.runtime.FileLocator
+    val bsHandle = project.getFile("configure.coqoon.py")
+    var copyScript : Option[Boolean] = None
+    if (!bsHandle.exists) {
+      copyScript = Some(true)
+    } else {
+      import java.io.{BufferedReader, InputStreamReader}
+      val r = new BufferedReader(new InputStreamReader(bsHandle.getContents))
+      try {
+        var line : Option[String] = None
+        do {
+          line.foreach(_ match {
+            case Version(version_) =>
+              val version = Integer.parseInt(version_)
+              copyScript = Some(version < currentVersion)
+            case _ =>
+          })
+          line = Option(r.readLine)
+        } while (copyScript == None && line != None)
+      } finally r.close
+    }
+    if (copyScript == Some(true)) {
+      val s = FileLocator.find(Activator.getDefault.getBundle,
+          new Path("lib/configure.coqoon.py"), null).openStream
+      if (bsHandle.exists) {
+        bsHandle.setContents(s, 0, null)
+      } else bsHandle.create(s, 0, null)
+    }
+    return copyScript.getOrElse(false)
+  }
+
+  def generateVars(project : ICoqProject) = {
+    import java.io.{BufferedWriter, OutputStreamWriter}
+    val vfHandle = project.getCorrespondingResource.map(
+        _.getFile("configure.coqoon.vars"))
+    vfHandle.foreach(vf => {
+      /* We could have use the IncompleteLoadPathEntry.Variable type here, but
+       * using a Map automatically cleans up the duplicates for us */
+      var vars : Map[String, String] = Map()
+      var alpBits : Seq[(String, String, String)] = Seq()
+      var alpNames : Map[String, String] = Map()
+      project.getLoadPathProviders.foreach(_ match {
+        case q @ AbstractLoadPath(id) =>
+          q.getImplementation.foreach(impl => {
+            alpNames += (id -> impl.getName)
+            for (parts <- impl.getIncompleteLoadPath.right;
+                 part <- parts) {
+              val stringisedPath =
+                part.path.foldLeft("")((s, a) => s + (a match {
+                  case Left(v) =>
+                    vars += (v.name -> v.description)
+                    "$(" + v.name + ")"
+                  case Right(s) =>
+                    s
+                }))
+              alpBits :+= ((id, part.coqdir.mkString("."), stringisedPath))
+            }
+          })
+        case _ =>
+      })
+      if (vars.size != 0 && alpBits.length != 0) {
+        var document =
+"""# Manipulating this project using Coqoon may cause this file to be overwritten
+# without warning: any local changes you may have made will not be preserved.
+
+"""
+        for ((name, description) <- vars)
+          document += s"""var "${name}" "${description}"\n"""
+        for ((aid, name) <- alpNames)
+          document += s"""alp "${aid}" name "${name}"\n"""
+        for ((aid, coqdir, path) <- alpBits)
+          document += s"""alp "${aid}" include-recursive "${path}" "${coqdir}"\n"""
+
+        import java.io.ByteArrayInputStream
+        val is = new ByteArrayInputStream(document.getBytes("UTF-8"))
+        if (vf.exists) {
+          vf.setContents(is, 0, null)
+        } else vf.create(is, 0, null)
+      }
+    })
+  }
+}
