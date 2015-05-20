coqoon/coqoon

commit e33bc34be5ffd233294ec8766cee501c88622921

Changed the way that LoadPathEntry represents coqdir values
The rest of the codebase -- including LoadPathEntry's own expand method! --
considers coqdirs to be Seq[String]s; this isolated use of Option[String]
was a peculiar and unhelpful anomaly

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqModel.scala
@@ -90,21 +90,21 @@ case class CoqProjectLoadPathChangedEvent(
     override val element : ICoqProject) extends CoqElementChangedEvent(element)
 
 final case class LoadPathEntry(
-    path : IPath, coqdir : Option[String], alwaysExpand : Boolean = false) {
+    path : IPath, coqdir : Seq[String], alwaysExpand : Boolean = false) {
   import dk.itu.coqoon.core.CoqoonPreferences
 
   def asCommand : String =
     (if (!alwaysExpand && CoqoonPreferences.EnforceNamespaces.get) {
       s"""Add LoadPath "${path.toOSString}" """
-    } else s"""Add Rec LoadPath "${path.toOSString}" """) + (coqdir match {
-      case Some(dir) => " as " + dir
-      case None => ""
-    }) + "."
+    } else s"""Add Rec LoadPath "${path.toOSString}" """) +
+    (if (coqdir != Nil) {
+      " as " + coqdir.mkString(".")
+    } else "") + "."
 
   def asArguments : Seq[String] =
     if (!alwaysExpand && CoqoonPreferences.EnforceNamespaces.get) {
       Seq("-I", path.toOSString) ++ coqdir.toSeq.flatMap(cd => Seq("-as", cd))
-    } else Seq("-R", path.toOSString, coqdir.getOrElse(""))
+    } else Seq("-R", path.toOSString, coqdir.mkString("."))
 
   import java.io.File
 
@@ -120,7 +120,7 @@ final case class LoadPathEntry(
             f => _recurse(coqdir :+ f.getName, f))
         } else Seq.empty) :+ (coqdir, f)
       }
-      _recurse(coqdir.map(_.split('.').toSeq).getOrElse(Seq()), path.toFile)
+      _recurse(coqdir, path.toFile)
     }
 }
 
@@ -136,16 +136,16 @@ case class ProjectLoadPath(
 
 case class SourceLoadPath(val folder : IFolder,
     val output : Option[IFolder] = None) extends ICoqLoadPathProvider {
-  override def getLoadPath = List(LoadPathEntry(folder.getLocation, None)) ++
-      output.map(a => LoadPathEntry(a.getLocation, None))
+  override def getLoadPath = List(LoadPathEntry(folder.getLocation, Nil)) ++
+      output.map(a => LoadPathEntry(a.getLocation, Nil))
 }
 
 case class DefaultOutputLoadPath(
     val folder : IFolder) extends ICoqLoadPathProvider {
-  override def getLoadPath = List(LoadPathEntry(folder.getLocation, None))
+  override def getLoadPath = List(LoadPathEntry(folder.getLocation, Nil))
 }
 
-case class ExternalLoadPath(val fsPath : IPath, val dir : Option[String])
+case class ExternalLoadPath(val fsPath : IPath, val dir : Seq[String])
     extends ICoqLoadPathProvider {
   override def getLoadPath = List(LoadPathEntry(fsPath, dir))
 }
@@ -266,9 +266,9 @@ object Coq84Library {
             val (_, path_) = CoqProgram("coqtop").run(Seq("-where")).readAll
             val path = new Path(path_.trim)
             Right(Seq(
-                LoadPathEntry(path.append("theories"), Some("Coq"), true),
-                LoadPathEntry(path.append("plugins"), Some("Coq"), true),
-                LoadPathEntry(path.append("user-contrib"), None, true)))
+                LoadPathEntry(path.append("theories"), Seq("Coq"), true),
+                LoadPathEntry(path.append("plugins"), Seq("Coq"), true),
+                LoadPathEntry(path.append("user-contrib"), Nil, true)))
           case _ => Left(Broken)
         }
       } else Left(VersionMismatch)

plugins/dk.itu.sdg.kopitiam/src/main/scala/dk/itu/sdg/kopitiam/javap/ChargeLibrary.scala
@@ -36,7 +36,7 @@ object ChargeLibrary {
       if (id == ID) {
         Activator.getDefault.getPreferenceStore.getString("loadpath") match {
           case p if p.length > 0 =>
-            Right(Seq(LoadPathEntry(new Path(p), None)))
+            Right(Seq(LoadPathEntry(new Path(p), Nil)))
           case _ =>
             Left(Broken)
         }

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqModelImpl.scala
@@ -236,10 +236,10 @@ private class CoqProjectImpl(
               SourceLoadPath(res.getFolder(srcdir),
                 Option(res.getFolder(bindir))) +: _util(tail)
             case "ExternalLoadPath" :: physical :: Nil =>
-              ExternalLoadPath(new Path(physical), None) +: _util(tail)
+              ExternalLoadPath(new Path(physical), Nil) +: _util(tail)
             case "ExternalLoadPath" :: physical :: logical :: Nil =>
               ExternalLoadPath(
-                  new Path(physical), Some(logical)) +: _util(tail)
+                  new Path(physical), logical.split(".")) +: _util(tail)
             case "AbstractLoadPath" :: identifier :: Nil =>
               AbstractLoadPath(identifier) +: _util(tail)
             case _ => _util(tail)
@@ -298,14 +298,14 @@ private class CoqProjectImpl(
           val path = bin.getProjectRelativePath.toString
           coqPart :+= RecursiveEntry(path, "")
           Seq("DefaultOutput", bin.getProjectRelativePath.toString)
-        case ExternalLoadPath(path_, None) =>
+        case ExternalLoadPath(path_, Nil) =>
           val path = path_.toString
           coqPart :+= RecursiveEntry(path, "")
           Seq("ExternalLoadPath", path)
-        case ExternalLoadPath(path_, Some(coqdir)) =>
+        case ExternalLoadPath(path_, coqdir) =>
           val path = path_.toString
-          coqPart :+= RecursiveEntry(path, coqdir)
-          Seq("ExternalLoadPath", path, coqdir)
+          coqPart :+= RecursiveEntry(path, coqdir.mkString("."))
+          Seq("ExternalLoadPath", path, coqdir.mkString("."))
         case ProjectLoadPath(project) =>
           val path = project.getName
           Seq("ProjectLoadPath", project.getName)

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/project/CoqMakefile.scala
@@ -55,7 +55,7 @@ include CoqoonMakefile.vars
 
     var identifiers = Map[String, ICoqLoadPathProvider]()
     def escape(lp : LoadPathEntry) = makePathRelative(lp.path).map(
-        a => Seq("-R", a.toString, lp.coqdir.getOrElse("")).map(
+        a => Seq("-R", a.toString, lp.coqdir.mkString(".")).map(
             CoqProjectEntry.escape)).get
     for (i <- cp.getLoadPathProviders) i match {
       case p @ DefaultOutputLoadPath(bin) =>
@@ -68,11 +68,15 @@ include CoqoonMakefile.vars
         sb ++= "override COQFLAGS += " +
             p.getLoadPath.flatMap(escape).mkString(" ") + "\n"
       case ExternalLoadPath(path, coqdir) =>
-        val name = getFirstIdent(coqdir.getOrElse("external") + "_path",
+        val base =
+          if (coqdir != Nil) {
+            coqdir.mkString(".")
+          } else "external"
+        val name = getFirstIdent(base + "_path",
             n => identifiers.contains(n))
         identifiers += (name -> i)
         sb ++= "override COQFLAGS += -R $(" + name + ") " +
-            CoqProjectEntry.escape(coqdir.getOrElse("")) + "\n"
+            CoqProjectEntry.escape(coqdir.mkString(".")) + "\n"
       case AbstractLoadPath(identifier) =>
         val name = getFirstIdent(identifier + "_flags",
             n => identifiers.contains(n))

plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/LoadPathUI.scala
@@ -93,7 +93,7 @@ protected object LoadPathModel {
 
   import org.eclipse.core.runtime.IPath
   case class ExternalLPE(parent : Option[LPProvider], fsPath : IPath,
-      dir : Option[String], index : Int) extends LPProvider(parent, index)
+      dir : Seq[String], index : Int) extends LPProvider(parent, index)
 
   case class OutputSLPE(parent : Option[LPProvider],
       output : Option[IFolder]) extends LPBase(parent)
@@ -161,7 +161,11 @@ private class LoadPathLabelProvider extends StyledCellLabelProvider {
 
         case NamespaceSLPE(_, lpe) =>
           s.append("Namespace: ")
-          s.append(lpe.coqdir.getOrElse("(root)"), ColourStyler(NSLOC))
+          val label =
+            if (lpe != Nil) {
+              lpe.coqdir.mkString(".")
+            } else "(root)"
+          s.append(label, ColourStyler(NSLOC))
         case LocationSLPE(_, lpe) =>
           s.append("Location: ")
           s.append(lpe.path.toString, ColourStyler(NSLOC))
@@ -612,8 +616,9 @@ class NLPExternalEntryPage extends NLPWizardPage(
 
   import org.eclipse.core.runtime.IPath
   private var fspath : Option[IPath] = None
-  private var dir : Option[String] = None
-  override def createLoadPathEntry = fspath.map(ExternalLoadPath(_, dir))
+  private var dir : Option[Seq[String]] = None
+  override def createLoadPathEntry =
+    fspath.map(ExternalLoadPath(_, dir.getOrElse(Seq())))
 
   override def createControl(parent : Composite) = {
     import org.eclipse.swt.layout.{GridData, GridLayout}

commit fe4c6ca0b53e95e965abaa4b179f94f57f57f3d9

Reworked the way Coqoon's optional namespace enforcement works
Because the -Q option can override previous -R options (or their moral
equivalents), this amounts to an enormous simplification of the code.
Hooray!

(Rather like commit 4c25c2eec587007a6d64ec3c96915dc556a10377, this commit
also fixes /another/ namespace enforcement bug introduced in commit
e33bc34be5ffd233294ec8766cee501c88622921.)

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/CoqoonPreferencesPage.scala
@@ -39,11 +39,14 @@ class CoqoonPreferencesPage
     })
     addField({
       val parent = getFieldEditorParent
-      val ed = new BooleanFieldEditor(CoqoonPreferences.EnforceNamespaces.ID,
-          "Enable namespace enforcement (experimental)", parent)
+      val ed = new BooleanFieldEditor(
+          CoqoonPreferences.RequireQualification.ID,
+          "Require library names to be qualified (experimental, Coq 8.5+)",
+          parent)
       ed.getDescriptionControl(parent).setToolTipText(
-          "Don't use the -R option to include project directories or " +
-          "dependencies.")
+          "Modify the dependency resolution behaviour to mirror that of the " +
+          "-Q option introduced in Coq 8.5, and pass that option when " +
+          "configuring Coq processes.")
       ed
     })
   }
@@ -68,7 +71,7 @@ class CoqoonPreferences extends AbstractPreferenceInitializer {
     val node = DefaultScope.INSTANCE.getNode(ManifestIdentifiers.PLUGIN)
 
     node.put(CoqPath.ID, CoqPath.tryCandidates.getOrElse(""))
-    node.putBoolean(EnforceNamespaces.ID, false)
+    node.putBoolean(RequireQualification.ID, false)
   }
 }
 object CoqoonPreferences {
@@ -94,8 +97,8 @@ object CoqoonPreferences {
     }
   }
 
-  object EnforceNamespaces {
-    final val ID = "enforce"
+  object RequireQualification {
+    final val ID = "requirequalification"
     def get() = Activator.getDefault.getPreferenceStore.getBoolean(ID)
   }
 }

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqLoadPath.scala
@@ -123,11 +123,11 @@ object SourceLoadPathProvider {
           IncompleteLoadPathEntry(
               Seq(Left(ProjectLocation),
                   Right(folder.getProjectRelativePath.toString)),
-              Nil, false)) ++
+              Nil)) ++
           output.map(output => IncompleteLoadPathEntry(
               Seq(Left(ProjectLocation),
                   Right(output.getProjectRelativePath.toString)),
-              Nil, false)))
+              Nil)))
 
     import IncompleteLoadPathEntry.Variable
     def getValue(v : Variable) =
@@ -195,7 +195,7 @@ object DefaultOutputLoadPathProvider {
           IncompleteLoadPathEntry(
               Seq(Left(ProjectLocation),
                   Right(folder.getProjectRelativePath.toString)),
-              Nil, false)))
+              Nil)))
 
     import IncompleteLoadPathEntry.Variable
     def getValue(v : Variable) =
@@ -263,7 +263,7 @@ object ExternalLoadPathProvider {
       Right(Seq(
           IncompleteLoadPathEntry(
               Seq(Left(ExternalPath)),
-              dir, false)))
+              dir)))
 
     import IncompleteLoadPathEntry.Variable
     def getValue(v : Variable) =

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/model/CoqModel.scala
@@ -90,12 +90,11 @@ case class CoqFileContentChangedEvent(
 case class CoqProjectLoadPathChangedEvent(
     override val element : ICoqProject) extends CoqElementChangedEvent(element)
 
-final case class LoadPathEntry(
-    path : IPath, coqdir : Seq[String], alwaysExpand : Boolean = false) {
+final case class LoadPathEntry(path : IPath, coqdir : Seq[String]) {
   import dk.itu.coqoon.core.CoqoonPreferences
 
   def asCommand : String =
-    (if (!alwaysExpand && CoqoonPreferences.EnforceNamespaces.get) {
+    (if (CoqoonPreferences.RequireQualification.get) {
       s"""Add LoadPath "${path.toOSString}" """
     } else s"""Add Rec LoadPath "${path.toOSString}" """) +
     (if (coqdir != Nil) {
@@ -104,22 +103,16 @@ final case class LoadPathEntry(
 
   def asArguments : Seq[String] = {
     val cd = coqdir.mkString(".")
-    if (!alwaysExpand && CoqoonPreferences.EnforceNamespaces.get) {
-      if (coqdir.isEmpty) {
-        Seq("-I", path.toOSString)
-      } else {
-        if (CoqProgram.version.exists(_.startsWith("8.4"))) {
-          Seq("-I", path.toOSString, "-as", cd)
-        } else Seq("-Q", path.toOSString, cd)
-      }
+    if (CoqoonPreferences.RequireQualification.get) {
+      Seq("-Q", path.toOSString, cd)
     } else Seq("-R", path.toOSString, cd)
   }
 
   import java.io.File
 
   def expand() : Seq[(Seq[String], File)] =
-    if (!alwaysExpand && CoqoonPreferences.EnforceNamespaces.get) {
-      Seq((coqdir.toSeq.flatMap(_.split('.')), path.toFile))
+    if (CoqoonPreferences.RequireQualification.get) {
+      Seq((coqdir, path.toFile))
     } else {
       def _recurse(
           coqdir : Seq[String], f : File) : Seq[(Seq[String], File)] = {
@@ -138,7 +131,7 @@ final case class LoadPathEntry(
 
 final case class IncompleteLoadPathEntry(
     path : Seq[Either[IncompleteLoadPathEntry.Variable, String]],
-    coqdir : Seq[String], alwaysExpand : Boolean = false) {
+    coqdir : Seq[String]) {
   import IncompleteLoadPathEntry._
   def complete(p : VariableProvider) :
       Either[IncompleteLoadPathEntry, LoadPathEntry] = {
@@ -155,7 +148,7 @@ final case class IncompleteLoadPathEntry(
     val v =
       if (t.forall(_.isRight)) {
         Right(LoadPathEntry(
-            new Path(t.map(_.right.get).mkString("/")), coqdir, alwaysExpand))
+            new Path(t.map(_.right.get).mkString("/")), coqdir))
       } else Left(IncompleteLoadPathEntry(t, coqdir))
     CoqoonDebugPreferences.LoadPathResolution.log(s"${this} -> ${v}")
     v
@@ -356,13 +349,13 @@ object CoqStandardLibrary {
             Right(Seq(
                 IncompleteLoadPathEntry(
                     Seq(Left(CoqLocation), Right("/theories")),
-                    Seq("Coq"), true),
+                    Seq("Coq")),
                 IncompleteLoadPathEntry(
                     Seq(Left(CoqLocation), Right("/plugins")),
-                    Seq("Coq"), true),
+                    Seq("Coq")),
                 IncompleteLoadPathEntry(
                     Seq(Left(CoqLocation), Right("/user-contrib")),
-                    Nil, true)))
+                    Nil)))
           case _ => Left(Broken)
         }
       } else Left(VersionMismatch)

plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/project/CoqCompiler.scala
@@ -87,14 +87,7 @@ class CoqCompilerRunner(source : IFile,
     val path = location.removeLastSegments(1).toOSString
     val cd = coqdir.mkString(".")
 
-    val owndirArguments =
-      if (coqdir.isEmpty) {
-        Seq("-I", path)
-      } else {
-        if (coqc.version.exists(_.startsWith("8.4"))) {
-          Seq("-I", path, "-as", cd)
-        } else Seq("-Q", path, cd)
-      }
+    val owndirArguments = Seq("-R", path, coqdir.mkString("."))
 
     val cp = ICoqModel.toCoqProject(source.getProject)
     val flp = cp.getLoadPath.flatMap(_.asArguments)

plugins/dk.itu.sdg.kopitiam/src/main/scala/dk/itu/sdg/kopitiam/javap/ChargeLibrary.scala
@@ -36,7 +36,7 @@ object ChargeLibrary {
       if (id == ID) {
         Right(Seq(
             IncompleteLoadPathEntry(Seq(
-                Left(ChargeLocation)), Nil, true)))
+                Left(ChargeLocation)), Nil)))
       } else Left(VersionMismatch)
 
     override def getValue(v : IncompleteLoadPathEntry.Variable) =