bc5bebf37b7208f273e56f7933c225fb9add0abd
Alec Faithfull <alef@itu.dk>
2015-07-23 14:09:01 +0200

    Merged the two SessionManager classes
    dk.itu.coqoon.ui.pide.SessionManager is the surviving class, which means
    that the PIDE plugin has returned to being a simple container

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/SessionManager.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/SessionManager.scala
@@ -12,10 +12,49 @@ import dk.itu.coqoon.core.debug.CoqoonDebugPreferences
 import dk.itu.coqoon.core.coqtop.CoqProgram
 import isabelle.Session
 
-class SessionManager extends dk.itu.coqoon.pide.SessionManager {
+class SessionManager {
+  import isabelle.Session
+  import dk.itu.coqoon.core.utilities.CacheSlot
+
+  private final val SessionLock = new Object
+  private lazy val session = CacheSlot {
+    import isabelle.{Coq_Syntax, Coq_Resources}
+    val syntax = new Coq_Syntax
+    val resources = new Coq_Resources(syntax)
+    val session = new Session
+    session.register_resources("coq", resources)
+
+    initialisers.foreach(f => f(session))
+
+    session
+  }
+
+  private var initialisers : Seq[Session => Unit] = Seq()
+  /* Adds the function @f to the list of functions to be called whenever a
+   * session is created or recreated. (If a session is already running, then @f
+   * will be called with it, but this method will not create a new session.) */
+  def addInitialiser(f : Session => Unit) =
+    executeWithSessionLockSlot(slot => {
+      initialisers :+= f
+      if (slot.test)
+        f(slot.get)
+    })
+
+  def stop() = executeWithSessionLockSlot(_.asOption.foreach(_.stop))
+
+  def getPhase() = executeWithSessionLock(_.phase)
+
+  def executeWithSessionLock[A](f : Session => A) =
+    executeWithSessionLockSlot(s => f(s.get))
+
+  def executeWithSessionLockSlot[A](f : CacheSlot[Session] => A) =
+    SessionLock synchronized {
+      f(session)
+    }
+
   import CoqoonDebugPreferences.PrintPIDETraffic
 
-  override def start =
+  def start() : Session.Phase =
     executeWithSessionLock(session => {
       session.start("coq", CoqProgram.path,
           Seq("-async-queries-always-delegate") ++
