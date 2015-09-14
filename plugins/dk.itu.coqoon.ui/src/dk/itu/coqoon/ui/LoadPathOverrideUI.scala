a62cd3bd72668e2cab0d28596a2df6a979caab9c
Alec Faithfull <alef@itu.dk>
2015-09-14 18:19:51 +0200

    Added a (dummy) preference page for configuring local overrides
    This will eventually be the front-end to the functionality added in commit
    0d044ee3aae1632c7cb60dd459857f33e916d6f8; at the moment, though, it's just
    a quick test of the new XML representation for UIs

---
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/LoadPathOverrideUI.scala
@@ -0,0 +1,49 @@
+package dk.itu.coqoon.ui
+
+import org.eclipse.ui.IWorkbenchPropertyPage
+import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.{Widget, Composite}
+import org.eclipse.core.runtime.IAdaptable
+import org.eclipse.jface.preference.PreferencePage
+
+class LoadPathOverridePreferencePage
+    extends PreferencePage with IWorkbenchPropertyPage {
+  override def getElement() = null
+  override def setElement(element : IAdaptable) = ()
+
+  import dk.itu.coqoon.ui.utilities.UIXML
+  override def createContents(parent : Composite) = {
+    import org.eclipse.jface.viewers.TableViewer
+    val names = UIXML.makeNameMap
+    UIXML(
+        <composite name="root">
+          <grid-layout columns="2" equal-width="false" />
+          <composite name="tv-container">
+            <grid-data h-grab="true" v-grab="true" />
+            <fill-layout />
+          </composite>
+          <composite>
+            <grid-data />
+            <grid-layout columns="1" />
+            <button>
+              <grid-data />
+              Add...
+            </button>
+            <label separator="horizontal">
+              <grid-data />
+            </label>
+            <button>
+              <grid-data />
+              Remove
+            </button>
+            <button enabled="false">
+              <grid-data />
+              Edit...
+            </button>
+          </composite>
+        </composite>, parent, Some(names))
+    val tvc = names.get("tv-container").get.asInstanceOf[Composite]
+    val tv = new TableViewer(tvc)
+    names.get("root").get.asInstanceOf[Composite]
+  }
+}
