921dcd64efb4ad025a366f70990822fdf92a70a4
Alec Faithfull <alef@itu.dk>
2015-09-15 16:43:51 +0200

    Added some utility objects for making partial functions into SWT listeners
    Even this early prototype drastically reduces the amount of code needed to
    add SWT listeners to widgets, so I'm obviously very happy about that

---
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/utilities/Listeners.scala
@@ -0,0 +1,22 @@
+package dk.itu.coqoon.ui.utilities
+
+import org.eclipse.swt.{SWT, events, widgets}
+
+object Event {
+  object Selection {
+    def unapply(ev : widgets.Event) =
+      if (ev.`type` == SWT.Selection) {
+        Some(new events.SelectionEvent(ev))
+      } else None
+  }
+}
+object Listener {
+  def apply(f : PartialFunction[widgets.Event, Unit]) =
+    new widgets.Listener {
+      override def handleEvent(ev : widgets.Event) = f(ev)
+    }
+  object Selection {
+    def apply(w : widgets.Widget, l : widgets.Listener) =
+      w.addListener(SWT.Selection, l)
+  }
+}
