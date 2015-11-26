9c5eca72b23e9a982c9e241006163e8c3dcba01f
Alec Faithfull <alef@itu.dk>
2015-11-26 16:01:32 +0100

    Allow PIDESessionHosts to go into a failed state
    When their associated Coq process dies unexpectedly, PIDESessionHosts will
    now print a message to the log and stop the process from being restarted ad
    nauseam

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/PIDESessionHost.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/PIDESessionHost.scala
@@ -30,7 +30,11 @@ trait PIDESessionHost extends OverlayRunner {
   protected def executeWithCommandsLock[A](f : => A) =
     CommandsLock synchronized f
 
-  session.addInitialiser(session =>
+  private object DeadLock /* (no pun intended) */ {
+    var dead : Boolean = false
+  }
+
+  session.addInitialiser(session => {
     session.commands_changed += Session.Consumer[Any]("Coqoon") {
       case changed : Session.Commands_Changed =>
         CommandsLock synchronized {
@@ -46,50 +50,68 @@ trait PIDESessionHost extends OverlayRunner {
           case _ =>
         }
       case _ =>
-    })
+    }
+    session.syslog_messages += Session.Consumer("Coqoon") {
+      case o : isabelle.Prover.Output
+          if o.is_exit && o.properties.contains("return_code" -> "1") =>
+        DeadLock synchronized {
+          DeadLock.dead = true
+          val lastLines = session.syslog_content.lines.toSeq.map(
+              _.trim).filterNot(_.isEmpty).reverse.take(50).reverse
+          dk.itu.coqoon.ui.utilities.EclipseConsole.err.println(
+              s"Coq session for ${getNodeName.getOrElse("(unknown)")} " +
+              "stopped unexpectedly:\n" + lastLines.mkString("\n"))
+          getAnnotationModel.foreach(_.removeAllAnnotations)
+          /* Raise an error? */
+        }
+      case _ =>
+    }
+  })
 
   def checkedUpdate(
       edits_ : List[Document.Node.Edit[Text.Edit, Text.Perspective]]) =
-    session.executeWithSessionLockSlot(slot => {
-      val submitInitialEdits =
-        slot.asOption match {
-          case None =>
-            session.start
-            true
-          case Some(s)
-              if s.phase == Session.Failed || s.phase == Session.Inactive =>
-            /* The prover stopped or was killed off, so any existing progress
-             * markers are invalid; clear them */
-            getAnnotationModel.foreach(_.removeAllAnnotations)
-            s.stop
-            slot.clear
-            session.start
-            true
-          case _ =>
-            false
+    if (!(DeadLock synchronized (DeadLock.dead))) {
+      session.executeWithSessionLockSlot(slot => {
+        val submitInitialEdits =
+          slot.asOption match {
+            case None =>
+              session.start
+              true
+            case Some(s)
+                if s.phase == Session.Failed || s.phase == Session.Inactive =>
+              /* The prover stopped or was killed off, so any existing progress
+               * markers are invalid; clear them */
+              getAnnotationModel.foreach(_.removeAllAnnotations)
+              s.stop
+              slot.clear
+              session.start
+              true
+            case _ =>
+              false
+          }
+        if (slot.get.phase == Session.Ready) {
+          val edits =
+            if (submitInitialEdits) {
+              /* Since the generateInitialEdits method builds an initial edit
+               * including the current body of the document, the text edits
+               * passed to this method are redundant! As a result, we need to
+               * filter those edits out to avoid getting out of sync. */
+              val eds = edits_.filterNot {
+                case e : Document.Node.Edits[
+                  Text.Edit, Text.Perspective] => true
+                case _ => false
+              }
+              generateInitialEdits ++ eds
+            } else edits_
+          getNodeName.foreach(nodeName => {
+            val textEdits : List[Document.Edit_Text] =
+              edits.map(e => nodeName -> e) :+ (nodeName -> getPerspective)
+            slot.get.update(Document.Blobs.empty, textEdits, "coq")
+          })
         }
-      if (slot.get.phase == Session.Ready) {
-        val edits =
-          if (submitInitialEdits) {
-            /* Since the generateInitialEdits method builds an initial edit
-             * including the current body of the document, the text edits
-             * passed to this method are redundant! As a result, we need to
-             * filter those edits out to avoid getting out of sync. */
-            val eds = edits_.filterNot {
-              case e : Document.Node.Edits[
-                Text.Edit, Text.Perspective] => true
-              case _ => false
-            }
-            generateInitialEdits ++ eds
-          } else edits_
-        getNodeName.foreach(nodeName => {
-          val textEdits : List[Document.Edit_Text] =
-            edits.map(e => nodeName -> e) :+ (nodeName -> getPerspective)
-          slot.get.update(Document.Blobs.empty, textEdits, "coq")
-        })
-      }
-      slot.get.phase
-    })
+        slot.get.phase
+      })
+    }
 
   /* Finds the PIDE command at @offset, if there is one, and returns it and its
    * offset in the complete PIDE document.
