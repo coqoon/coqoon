e89c5792d9a63a6560a03b36e1bb2b7e117d8b2a
Alec Faithfull <alef@itu.dk>
2016-03-15 17:14:15 +0100

    Removed the PIDENavigationHost trait and its companion object
    From now on, anything that knows about Coq entities should register that
    knowledge with the model

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/PIDECoqEditor.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/pide/PIDECoqEditor.scala
@@ -6,64 +6,8 @@ import dk.itu.coqoon.ui.text.Region
 import dk.itu.coqoon.ui.utilities.SupersedableTask
 import dk.itu.coqoon.core.model.{CoqEnforcement, CoqEnforcementContext}
 
-trait PIDENavigationHost extends PIDESessionHost {
-  def getEntities(command : isabelle.Command) :
-      Seq[(isabelle.Text.Range, Responses.Entity)]
-  def selectEntity(e : Responses.Entity) : Unit
-}
-object PIDENavigationHost {
-  def openLeft(left : (String, (Int, Int))) = {
-    val (path_, (start, end)) = left
-    import dk.itu.coqoon.ui.utilities.UIUtils
-    import dk.itu.coqoon.core.utilities.TryAdapt
-    import org.eclipse.ui.ide.IDE
-    import org.eclipse.core.runtime.Path
-    import org.eclipse.core.resources.ResourcesPlugin
-    import org.eclipse.core.filesystem.EFS
-    import org.eclipse.jface.text.source.ISourceViewer
-    val path = new Path(path_)
-    val file = EFS.getLocalFileSystem.getStore(path)
-    val page =
-      UIUtils.getWorkbench.getActiveWorkbenchWindow.getActivePage
-    Option(IDE.openInternalEditorOnFileStore(page, file)).
-        flatMap(TryAdapt[ISourceViewer]) match {
-      case Some(viewer) =>
-        val s = start - 1
-        val l = end - s
-        viewer.revealRange(s, l)
-        viewer.setSelectedRange(s, l)
-      case _ =>
-    }
-  }
-}
-
 class PIDECoqEditor
-    extends BaseCoqEditor with CoqGoalsContainer with PIDESessionHost
-                          with PIDENavigationHost {
-  override def getEntities(command : isabelle.Command) = {
-    val ls = getLastSnapshot.get
-      for ((range, elem) <- Responses.extractMarkup(ls, command);
-           entity <- Responses.extractEntity(elem))
-        yield (range, entity)
-  }
-  override def selectEntity(e : Responses.Entity) = {
-    val ls = getLastSnapshot.get
-    e match {
-      case Left(l) => PIDENavigationHost.openLeft(l)
-      case Right((exec_id, (start, end))) =>
-        ls.state.find_command(ls.version, exec_id) foreach {
-          case (_, command) =>
-            ls.node.command_start(command).filter(_ >= ibLength).map(
-                _ - ibLength).foreach(offset => {
-                val s = (offset + start) - 1
-                val l = end - start
-                getSourceViewer.revealRange(s, l)
-                getSourceViewer.setSelectedRange(s, l)
-              })
-        }
-    }
-  }
-
+    extends BaseCoqEditor with CoqGoalsContainer with PIDESessionHost {
   import org.eclipse.jface.text.IViewportListener
   object ViewportListener extends IViewportListener {
     val task = new SupersedableTask(200)
