14c784bdc460ca855fd8910b5adf3201f4b6bb02
Alec Faithfull <alef@itu.dk>
2016-03-16 17:24:43 +0100

    Added syntax error support to CoqBuilder's regular expression for errors

--- plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/project/CoqBuilder.scala
+++ plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/project/CoqBuilder.scala
@@ -514,7 +514,7 @@ private object CoqBuilder {
   private val GeneralError = """(?ms)Error: (.*)$""".r.unanchored
   private val CompilationError =
     ("""(?s)File "(.*)", line (\d+), characters (\d+)-(\d+):""" +
-     """\s+Error:\s+(.*)$""").r.unanchored
+     """\s+(?:Syntax |)Error:\s+(.*)$""").r.unanchored
 
   def cleanProject(project : ICoqProject) : Unit =
     for (i <- project.getLoadPathProviders) i match {
