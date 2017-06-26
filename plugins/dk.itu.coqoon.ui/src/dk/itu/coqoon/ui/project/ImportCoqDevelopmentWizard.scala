911d9d23b432bab7321d784a0230aa25110a3adc
Alec Faithfull <alef@itu.dk>
2017-06-26 17:29:09 +0200

    Drastically simplified the Coq development import wizard
    Reimplementing most of the load path UI in here seems like a bad idea; this
    new simpler version is a better place to start

--- plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/project/ImportCoqDevelopmentWizard.scala
+++ plugins/dk.itu.coqoon.ui/src/dk/itu/coqoon/ui/project/ImportCoqDevelopmentWizard.scala
@@ -7,6 +7,7 @@ import org.eclipse.ui.{IWorkbench, IImportWizard}
 import org.eclipse.ui.model.{WorkbenchLabelProvider, WorkbenchContentProvider}
 import org.eclipse.swt.SWT
 import org.eclipse.swt.widgets.{Text, Button, Control, Composite}
+import org.eclipse.core.runtime.Path
 import org.eclipse.jface.wizard.{Wizard, WizardPage}
 import org.eclipse.jface.viewers.{
   Viewer, CellEditor, TreeViewer, ViewerFilter, ComboBoxCellEditor,
@@ -30,74 +31,65 @@ class ImportCoqDevelopmentWizardPage extends WizardPage("icdwp") {
   setDescription(
       "Load an existing non-Coqoon Coq development into the workspace.")
 
-  private[project] var directory : Option[File] = None
-  private[project] var dispositions : Map[File, Int] = Map()
-
-  import org.eclipse.jface.viewers.{BaseLabelProvider, ITableLabelProvider}
-  class FilesystemLabelProvider
-      extends BaseLabelProvider with ITableLabelProvider {
-    import dk.itu.coqoon.ui.utilities.UIUtils.getWorkbench
-    import org.eclipse.ui.ISharedImages
-    def getColumnImage(el : AnyRef, column : Int) = (el, column) match {
-      case (f : File, 0) if f.isDirectory =>
-        getWorkbench.getSharedImages.getImage(ISharedImages.IMG_OBJ_FOLDER)
-      case (f : File, 0) if f.isFile =>
-        getWorkbench.getSharedImages.getImage(ISharedImages.IMG_OBJ_FILE)
-      case _ =>
-        null
-    }
-    override def isLabelProperty(el : AnyRef, property : String) =
-      (el, property) match {
-        case (f : File, "disposition") =>
-          true
-        case _ =>
-          false
-      }
-    def getColumnText(el : AnyRef, column : Int) = (el, column) match {
-      case (f : File, 0) =>
-        f.getName
-      case (f : File, 1) =>
-        ImportCoqDevelopmentWizardPage.dispositionLabels(
-            dispositions.get(f).getOrElse(0))
-      case _ =>
-        null
-    }
-  }
+  private[project] var directory : Option[Path] = None
+  private[project] var name : Option[String] = None
+  private[project] var source : Option[Path] = None
 
   override def createControl(parent : Composite) = {
     val names = UIXML(
         <composite name="root">
-          <grid-layout columns="3" />
+          <grid-layout columns="3" spacing="5" margin="5" />
           <label>
-            Directory:
+            Development:
           </label>
           <text name="dt" border="true">
             <grid-data h-align="fill" h-grab="true" />
           </text>
-          <button name="db">
+          <button>
+            <listener kind="select-directory" target="dt" />
             Browse...
           </button>
-          <tree-viewer name="tv">
-            <grid-data align="fill" grab="true" h-span="3" height-hint="300" />
-            <column label="Folder" style="left" />
-            <column label="Type" style="left" />
-          </tree-viewer>
+          <label separator="horizontal">
+            <grid-data h-grab="true" h-align="fill" h-span="3" />
+          </label>
+          <label name="nl" enabled="false">
+            Project name:
+          </label>
+          <text name="nt" border="true" enabled="false">
+            <grid-data h-align="fill" h-grab="true" h-span="2" />
+          </text>
+          <label name="sl" enabled="false">
+            Main source folder:
+          </label>
+          <text name="st" border="true" enabled="false">
+            <grid-data h-align="fill" h-grab="true" />
+          </text>
+          <button name="sb" enabled="false">
+            <listener kind="select-directory" target="st" />
+            Browse...
+          </button>
+          <label enabled="false" wrap="true" align="center" name="extra">
+            Other project settings can be configured from the Coq Load Path
+            property page once a project has been created for this development.
+            <grid-data align="fill" grab="true" h-span="3" width-hint="300" />
+          </label>
         </composite>, parent)
     setControl(names.get[Composite]("root").get)
     import dk.itu.coqoon.ui.utilities.{Event, UIUtils, Listener}
     val dt = names.get[Text]("dt").get
-    val db = names.get[Button]("db").get
-    val tv = names.get[TreeViewer]("tv").get
+    val nt = names.get[Text]("nt").get
+    val st = names.get[Text]("st").get
 
     Listener.Modify(dt, Listener {
       case Event.Modify(_) =>
         val path = dt.getText.trim
         directory =
           if (path.length > 0) {
-            Some(new File(path))
+            Some(new Path(path))
           } else None
-        directory match {
-          case Some(f : File) if f.exists && f.isDirectory =>
+        val file = directory.map(_.toFile)
+        file match {
+          case Some(f) if f.exists && f.isDirectory =>
             val files = f.list()
             if (files.contains(".project") ||
                 files.contains(".coqoonProject")) {
@@ -108,7 +100,7 @@ class ImportCoqDevelopmentWizardPage extends WizardPage("icdwp") {
             } else {
               setErrorMessage(null)
             }
-          case Some(f : File) if f.exists =>
+          case Some(f) if f.exists =>
             directory = None
             setErrorMessage(
                 s"""The path "$path" is not a directory""")
@@ -116,100 +108,64 @@ class ImportCoqDevelopmentWizardPage extends WizardPage("icdwp") {
             directory = None
             setErrorMessage(s"""The path "$path" does not exist""")
         }
-        if (getErrorMessage == null) {
-          directory.foreach(tv.setInput)
-        } else tv.setInput(null)
-        dispositions = Map()
-        tv.refresh()
+        names.getMany[Control](
+            "nl", "nt", "sl", "st", "sb", "extra").flatten.foreach(
+                _.setEnabled(directory != None))
+        if (directory != None) {
+          file.foreach(directory => {
+            nt.setText(directory.getName)
+            ImportCoqDevelopmentWizardPage.candidateDirectories.find(
+                d => directory.list().contains(d)).foreach(
+                    cd => st.setText(new File(directory, cd).toString))
+          })
+        }
         getContainer.updateButtons
     })
 
-    Listener.Selection(db, Listener {
-      case Event.Selection(_) =>
-        UIUtils.Dialog.directory(
-            "Select a directory",
-            "Select a directory containing a Coq development.").foreach(
-                dt.setText)
+    Listener.Modify(nt, Listener {
+      case Event.Modify(_) =>
+        val name = nt.getText.trim
+        if (name == "fred") {
+          this.name = None
+          setErrorMessage("Not fred")
+        } else {
+          this.name = Some(name)
+          setErrorMessage(null)
+        }
+        getContainer.updateButtons
     })
 
-    tv.getTree.setHeaderVisible(true)
-    tv.setContentProvider(new FilesystemContentProvider)
-    tv.setLabelProvider(new FilesystemLabelProvider)
-    tv.setFilters(Array(
-        FilesystemExcludeHiddenFilter, FilesystemDirectoryFilter))
-    tv.setColumnProperties(Array("resource", "disposition"))
-    val cbce = new ComboBoxCellEditor(
-        tv.getTree, ImportCoqDevelopmentWizardPage.dispositionLabels,
-        SWT.BORDER | SWT.READ_ONLY)
-    tv.setCellEditors(Array[CellEditor](null, cbce))
-    tv.setCellModifier(new org.eclipse.jface.viewers.ICellModifier {
-      override def canModify(el : AnyRef, property : String) =
-        (property == "disposition")
-      override def getValue(el : AnyRef, property : String) =
-        (el, property) match {
-          case (f : File, "disposition") =>
-            Int.box(dispositions.get(f).getOrElse(0))
-          case _ => null
-        }
-      override def modify(el_ : AnyRef, property : String, value : AnyRef) = {
-        val el = el_ match {
-          case i : org.eclipse.swt.widgets.Item => i.getData
-          case i => i
-        }
-        (el, property, value) match {
-          case (f : File, "disposition", i : Integer) =>
-            dispositions += (f -> i)
-            tv.update(f, Array("disposition"))
+    Listener.Modify(st, Listener {
+      case Event.Modify(_) =>
+        val path = st.getText.trim
+        source =
+          if (path.length > 0) {
+            Some(new Path(path))
+          } else None
+        val file = source.map(_.toFile)
+        (directory, source, file) match {
+          case (Some(d), Some(s), Some(f))
+              if d.isPrefixOf(s) && f.isDirectory =>
+            setErrorMessage(null)
+          case (Some(d), _, Some(f)) if f.isDirectory =>
+            source = None
+            setErrorMessage(
+                s"""The path "$path" is not within the development""")
+          case (_, _, Some(f)) if f.exists =>
+            source = None
+            setErrorMessage(
+                s"""The path "$path" is not a directory""")
           case _ =>
+            source = None
+            setErrorMessage(s"""The path "$path" does not exist""")
         }
-      }
+        getContainer.updateButtons
     })
-
-    for (i <- tv.getTree.getColumns)
-      i.pack
   }
 
-  override def isPageComplete() = (directory != None)
+  override def isPageComplete() =
+    (directory != None && name != None && source != None)
 }
-object ImportCoqDevelopmentWizardPage {
-  final val dispositionLabels = Array[String]("", "Coq source folder")
-}
-
-class FilesystemContentProvider extends ITreeContentProvider {
-  override def dispose() = ()
-  override def inputChanged(
-      v: Viewer, oldInput: AnyRef, newInput: AnyRef) = ()
-  override def getChildren(el: AnyRef) : Array[Object] =
-    TryCast[File](el) match {
-      case Some(f) if f.isDirectory =>
-        f.listFiles.sortBy(f => (f.isFile, f.getName.toLowerCase)).toArray
-      case _ =>
-        Array.empty
-    }
-  override def getElements(el: AnyRef) = getChildren(el)
-  override def getParent(el: AnyRef) =
-    TryCast[File](el).map(_.getParentFile).orNull
-  override def hasChildren(el: AnyRef) =
-    TryCast[File](el).flatMap(f => Option(f.list)).map(
-        !_.isEmpty).getOrElse(false)
-}
-
-object FilesystemExcludeHiddenFilter extends ViewerFilter {
-  override def select(viewer : Viewer, parent : AnyRef, el : AnyRef) =
-    el match {
-      case f : File if f.isHidden() =>
-        false
-      case _ =>
-        true
-    }
-}
-
-object FilesystemDirectoryFilter extends ViewerFilter {
-  override def select(viewer : Viewer, parent : AnyRef, el : AnyRef) =
-    el match {
-      case f : File if f.isDirectory() =>
-        true
-      case _ =>
-        false
-    }
+private object ImportCoqDevelopmentWizardPage {
+  private val candidateDirectories = Seq("theories", "src")
 }
