3ebb0ae6a7f5a20ad2f3b9b44eba43aabdf7a0ab
Alec Faithfull <alef@itu.dk>
2018-11-23 14:42:56 +0100

    Added a mechanism for creating Coq programs without a preference store
    This is mostly useful for testing purposes while I try to get the coqidetop
    protocol up and running

--- plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/coqtop/CoqProgram.scala
+++ plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/coqtop/CoqProgram.scala
@@ -8,29 +8,52 @@ trait CoqProgram {
   def check() : Boolean = new java.io.File(path).exists
   def run(args : Seq[String],
       start : ProcessBuilder => Process = (a => a.start)) : CoqProgramInstance
+  def version() = run(Seq("-v")).readAll match {
+    case (0, CoqProgram.Version(v)) =>
+      Some(v)
+    case _ =>
+      None
+  }
 }
 object CoqProgram extends CoqProgram {
-  private class ProgramImpl(name : String) extends CoqProgram {
-    override def path : String = CoqoonPreferences.CoqPath.get match {
-      case Some(path) => path + java.io.File.separator + name
-      case _ => name
-    }
+  private final val Version = raw"version (.+) ".r.unanchored
+
+  private trait RunPOSIXProgram extends CoqProgram {
     override def run(args : Seq[String],
         start : ProcessBuilder => Process) : CoqProgramInstance =
       new CoqProgramInstanceImplPOSIX(path +: args, start)
   }
-
-  private class ProgramImplWindows(
-      name : String) extends ProgramImpl(name + ".exe") {
+  private trait RunWindowsProgram extends CoqProgram {
     override def run(args : Seq[String],
         start : ProcessBuilder => Process) : CoqProgramInstance =
       new CoqProgramInstanceImplWindows(path +: args, start)
   }
 
+  private abstract class PathFromPreferencesProgram(
+      name : String) extends CoqProgram {
+    override def path : String = CoqoonPreferences.CoqPath.get match {
+      case Some(path) => path + java.io.File.separator + name
+      case _ => name
+    }
+  }
+  private abstract class FixedPathProgram(path_ : String) extends CoqProgram {
+    override def path : String = path_
+  }
+
+  private class PrefProgramImpl(name : String)
+      extends PathFromPreferencesProgram(name) with RunPOSIXProgram
+  private class PrefProgramImplWindows(name : String)
+      extends PathFromPreferencesProgram(name + ".exe") with RunWindowsProgram
+
+  private class FixedProgramImpl(path_ : String)
+      extends FixedPathProgram(path_) with RunPOSIXProgram
+  private class FixedProgramImplWindows(path_ : String)
+      extends FixedPathProgram(path_) with RunWindowsProgram
+
   private lazy val program : CoqProgram =
     if (PlatformUtilities.isWindows) {
-      new ProgramImplWindows("coqtop")
-    } else new ProgramImpl("coqtop")
+      new PrefProgramImplWindows("coqtop")
+    } else new PrefProgramImpl("coqtop")
 
   override def path = program.path
   override def check = program.check
@@ -38,14 +61,10 @@ object CoqProgram extends CoqProgram {
       start : ProcessBuilder => Process = (a => a.start)) =
     program.run(args, start)
 
-  def version() = run(Seq("-v")).readAll match {
-    case (0, Version(v)) =>
-      Some(v)
-    case _ =>
-      None
-  }
-
-  private final val Version = raw"version (.+) ".r.unanchored
+  def makeProgram(path : String) : CoqProgram =
+    if (PlatformUtilities.isWindows) {
+      new FixedProgramImplWindows(path + ".exe")
+    } else new FixedProgramImpl(path)
 }
 
 trait CoqProgramInstance {
