0dd390772ba414437c6d795b59f4992e2ab00587
Alec Faithfull <alef@itu.dk>
2018-12-06 11:52:42 +0100

    StateTracker should submit commands for execution on a background thread
    The thread in question is driven by a BatchCollector, so commands can be
    queued up for bulk execution without having to wait for responses

--- plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/coqtop/coqidetop/StateTracker.scala
+++ plugins/dk.itu.coqoon.core/src/dk/itu/coqoon/core/coqtop/coqidetop/StateTracker.scala
@@ -9,47 +9,63 @@ import dk.itu.coqoon.core.model.{ICoqScriptGroup, ICoqScriptSentence}
 import dk.itu.coqoon.core.model.CoqElementChangeListener
 import dk.itu.coqoon.core.model.{CoqElementEvent, CoqFileContentChangedEvent}
 import dk.itu.coqoon.core.coqtop.CoqSentence
+import dk.itu.coqoon.core.utilities.BatchCollector
 
 class StateTracker(
     ct : CoqIdeTop_v20170413) {
   import StateTracker._
 
-  private var file : Option[ICoqVernacFile] = None
-  private var initialisationBlock : Option[String] = None
-  private var stateIds : Seq[(SentenceID, Interface.state_id)] = Seq()
+  var file : Option[ICoqVernacFile] = None
+  var initialisationBlock : Option[String] = None
 
-  private var goals : Map[Interface.state_id, Interface.goals] = Map()
-  private var status :
-      Map[Interface.state_id, Interface.value[Interface.status]] = Map()
-  private var feedback : Map[Interface.state_id, MBuffer[Feedback]] = Map()
+  private object Lock {
+    var stateIds : Seq[(SentenceID, Interface.state_id)] = Seq()
+    var goals : Map[Interface.state_id, Interface.goals] = Map()
+    var status :
+        Map[Interface.state_id, Interface.value[Interface.status]] = Map()
+    var feedback : Map[Interface.state_id, MBuffer[Feedback]] = Map()
+    var privateState = -1
+  }
+
+  private def nextPrivateState() : Interface.state_id =
+    Lock synchronized (try Lock.privateState finally Lock.privateState -= 1)
 
   private def sendInitialisationBlock() =
     initialisationBlock.foreach(ib => {
-      CoqSentence.getNextSentences(ib, 0, ib.length).foreach(
-          s => submit(Left(s)))
+      CoqSentence.getNextSentences(ib, 0, ib.length).foreach(Submitter.add)
     })
 
   def sentenceKnown(s : ICoqScriptSentence) = (getStateID(s) != None)
 
+  private def _getStateID(
+      sentenceID : SentenceID) : Option[Interface.state_id] =
+    Lock synchronized {
+      import Lock._
+      stateIds.find(_._1 == sentenceID).map(_._2)
+    }
   private def getStateID(
-      s : ICoqScriptSentence) : Option[Interface.state_id] = {
-    val sentenceID = makeSentenceID(s)
-    stateIds.find(_._1 == sentenceID).map(_._2)
-  }
+      s : ICoqScriptSentence) : Option[Interface.state_id] =
+    _getStateID(makeSentenceID(s))
+  private def getStateID(
+      s : CoqSentence.Sentence) : Option[Interface.state_id] =
+    _getStateID(makeSentenceID(s))
 
-  def getGoals(s : ICoqScriptSentence) = getStateID(s).flatMap(goals.get)
-  def getStatus(s : ICoqScriptSentence) = getStateID(s).flatMap(status.get)
-  def getFeedback(s : ICoqScriptSentence) = getStateID(s).flatMap(feedback.get)
+  def getGoals(s : ICoqScriptSentence) =
+    Lock synchronized getStateID(s).flatMap(Lock.goals.get)
+  def getStatus(s : ICoqScriptSentence) =
+    Lock synchronized getStateID(s).flatMap(Lock.status.get)
+  def getFeedback(s : ICoqScriptSentence) =
+    Lock synchronized getStateID(s).flatMap(Lock.feedback.get)
 
   private def removeData(sids : Seq[Interface.state_id]) =
-    sids.foreach(sid => {
-      goals -= sid
-      status -= sid
-      feedback -= sid
+    Lock synchronized sids.foreach(sid => {
+      Lock.goals -= sid
+      Lock.status -= sid
+      Lock.feedback -= sid
     })
 
   private def getHead() : Interface.state_id =
-    stateIds.lastOption.map(_._2).getOrElse(1)
+    Lock synchronized Lock.stateIds.lastOption.map(_._2).getOrElse(1)
 
   private object ChangeListener
       extends CoqElementChangeListener with CoqIdeTopFeedbackListener {
@@ -57,9 +73,11 @@ class StateTracker(
       ev match {
         case CoqFileContentChangedEvent(
             f : ICoqVernacFile) if file.contains(f) =>
-          val zippedIDs = f.getSentences.map(
-              s => Some(makeSentenceID(s))).zipAll(
-                  stateIds.map(s => Some(s._1)), None, None)
+          val zippedIDs = Lock synchronized {
+            f.getSentences.map(
+                s => Some(makeSentenceID(s))).zipAll(
+                    Lock.stateIds.map(s => Some(s._1)), None, None)
+          }
           val divergence = zippedIDs.indexWhere {
             case (a, b) if a != b =>
               true
@@ -67,22 +85,28 @@ class StateTracker(
               false
           }
           if (divergence != -1) {
-            removeData(stateIds.drop(divergence).map(_._2))
-            stateIds = stateIds.take(divergence)
+            Lock synchronized {
+              removeData(Lock.stateIds.drop(divergence).map(_._2))
+              Lock.stateIds = Lock.stateIds.take(divergence)
+            }
             ct.editAt(getHead)
             if (getHead == 1)
               sendInitialisationBlock
-            submitAll(f.getSentences.drop(divergence))
+            f.getSentences.drop(divergence).foreach(Submitter.add)
           }
         case _ =>
     }
     override def onFeedback(f : Feedback) = {
-      /* Feedback might arrive before we've processed the return value of
-       * add(), so instead of going through stateIds to work out which command
-       * this message is associated with, just stash it away immediately */
-      if (!feedback.contains(f.state))
-        feedback += (f.state -> MBuffer())
-      feedback.get(f.state).get += f
+      Lock synchronized {
+        import Lock._
+
+        /* Feedback might arrive before we've processed the return value of
+         * add(), so instead of going through stateIds to work out which command
+         * this message is associated with, just stash it away immediately */
+        if (!feedback.contains(f.state))
+          feedback += (f.state -> MBuffer())
+        feedback.get(f.state).get += f
+      }
 
       /* Resend this feedback to our own listeners, with the expectation that
        * they'll basically ignore its content and will use a SupersedableTask
@@ -93,63 +117,71 @@ class StateTracker(
   }
   ct.addListener(ChangeListener)
 
-  def submitAll(sentences_ : Seq[ICoqScriptSentence]) = {
-    var sentences = sentences_
-    var continuing = true
-    while (continuing && !sentences.isEmpty) {
-      continuing = submit(Right(sentences.head))
-      sentences = sentences.tail
-    }
-  }
-  def submit(
-      s : Either[CoqSentence.Sentence, ICoqScriptSentence]) : Boolean = {
-    val (text, synthetic) = s match {
-      case Left((text, syn)) => (text, syn)
-      case Right(s) => (s.getText, s.isSynthetic)
-    }
-    if (!synthetic) {
-      ct.add(getHead, text, true) match {
-        case Interface.Good((sid, (Left(()), msg))) =>
-          stateIds :+= (makeSentenceID(s) -> sid)
-          val st = ct.status(false)
-          status += (sid -> st)
-          st match {
-            case Interface.Good(Interface.status(_, Some(_), _, _)) =>
-              ct.goal() match {
-                case Interface.Good(Some(gs)) =>
-                  goals += (sid -> gs)
-                case _ =>
-              }
-            case _ =>
-          }
-          true
-        case Interface.Fail((sid, loc, msg)) =>
-          /* Adding this state to the document failed, so it doesn't have a
-           * state ID -- and we need one to associate the error with. Make up
-           * for this by inventing a fake state ID for this command */
-          val ps = nextPrivateState
-          stateIds :+= (makeSentenceID(s) -> ps)
-          status += (ps -> Interface.Fail(sid, loc, msg))
+  private object Submitter extends BatchCollector[
+      Either[CoqSentence.Sentence, ICoqScriptSentence]] {
+    def add(s : CoqSentence.Sentence) : Unit = add(Left(s))
+    def add(s : ICoqScriptSentence) : Unit = add(Right(s))
 
-          s.right.foreach(s => {
-            /* Try to create an error marker */
-            val (pos, len) = loc match {
-              case Some((start, stop)) =>
-                (start, stop - start)
-              case None =>
-                (0, s.getLength)
-            }
-            val issue = CoqEnforcement.Issue(
-                ERR_ADD_FAILED, pos, len, msg, CoqEnforcement.Severity.Error)
-            s.addIssue((issue, CoqEnforcement.Severity.Error))
-          })
+    def process(sentences :
+        List[Either[CoqSentence.Sentence, ICoqScriptSentence]]) =
+      sentences.foreach(submit)
 
-          /* Stop processing commands here */
-          false
-        case _ =>
-          true
+    def submit(
+        s : Either[CoqSentence.Sentence, ICoqScriptSentence]) = {
+      val (text, synthetic) = s match {
+        case Left((text, syn)) => (text, syn)
+        case Right(s) => (s.getText, s.isSynthetic)
       }
-    } else true
+      if (!synthetic) {
+        ct.add(getHead, text, true) match {
+          case Interface.Good((sid, (Left(()), msg))) =>
+            val st = ct.status(false)
+            val goalData = st match {
+              case Interface.Good(Interface.status(_, Some(_), _, _)) =>
+                ct.goal() match {
+                  case Interface.Good(Some(gs)) =>
+                    Some(gs)
+                  case _ =>
+                    None
+                }
+              case _ =>
+                None
+            }
+
+            Lock synchronized {
+              import Lock._
+              stateIds :+= (makeSentenceID(s) -> sid)
+              status += (sid -> st)
+              goalData.foreach(gs => goals += (sid -> gs))
+            }
+            true
+          case Interface.Fail((sid, loc, msg)) =>
+            s.right.foreach(s => {
+              /* Try to create an error marker */
+              val (pos, len) = loc match {
+                case Some((start, stop)) =>
+                  (start, stop - start)
+                case None =>
+                  (0, s.getLength)
+              }
+              val issue = CoqEnforcement.Issue(
+                  ERR_ADD_FAILED, pos, len, msg, CoqEnforcement.Severity.Error)
+              s.addIssue((issue, CoqEnforcement.Severity.Error))
+            })
+
+            Lock synchronized {
+              import Lock._
+              /* Adding this state to the document failed, so it doesn't have a
+               * state ID -- and we need one to associate the error with. Make
+               * up for this by inventing a fake state ID for this command */
+              val ps = nextPrivateState
+              stateIds :+= (makeSentenceID(s) -> ps)
+              status += (ps -> Interface.Fail(sid, loc, msg))
+            }
+          case _ =>
+        }
+      }
+    }
   }
 
   def attach(f : ICoqVernacFile, ib : String = "") = {
@@ -158,9 +190,10 @@ class StateTracker(
     file = Some(f)
     f.getModel.addListener(ChangeListener)
     sendInitialisationBlock()
-    submitAll(f.getSentences)
+    f.getSentences.foreach(Submitter.add)
   }
-  def detach() = file.foreach(f => {
+  def detach() = file.foreach(f => Lock synchronized {
+    import Lock._
     f.getModel.removeListener(ChangeListener)
     file = None
     stateIds = Seq()
@@ -170,9 +203,8 @@ class StateTracker(
   })
 
   def query(s : ICoqScriptSentence,
-      query : String) : Either[String, Seq[String]] = {
-    val stid = makeSentenceID(s)
-    stateIds.find(_._1 == stid).map(_._2) match {
+      query : String) : Either[String, Seq[String]] =
+    getStateID(s) match {
       case Some(sid) =>
         val route = 32 + (scala.math.random * 32768.0).asInstanceOf[Int]
         var messages : Seq[String] = Seq()
@@ -195,11 +227,6 @@ class StateTracker(
       case None =>
         Left("Command not yet assigned a state by Coq")
     }
-  }
-
-  private var privateState = -1
-  private def nextPrivateState() : Interface.state_id =
-    try privateState finally privateState -= 1
 
   protected var listeners : Set[CoqIdeTopFeedbackListener] = Set()
   def addListener(l : CoqIdeTopFeedbackListener) = (listeners += l)
